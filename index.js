94afe3a896f8d4f23603f003facd20be30d547cd
sean chan <sean@seans-MacBook-Air.local>
2019-01-08 17:07:47 +0800

    MVP done

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 console.log("works!!", process.argv[2]);
 
+let input = process.argv[2];
+let inputArr = [];
+for(let i = 3; i < process.argv.length; i++){
+    inputArr.push(process.argv[i]);
+}
+console.log(inputArr)
+
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
+    user: 'sean',
     host: '127.0.0.1',
     database: 'todo',
     port: 5432,
@@ -11,11 +18,33 @@ const configs = {
 
 const client = new pg.Client(configs);
 
+let queryShowCallback = (err) => {
+    client.connect((err) => {
+
+      if( err ){
+        console.log( "error", err.message );
+      }
+
+      let text = 'SELECT * FROM items';
+
+      client.query(text, (err, result) => {
+        if (err) {
+          console.log("query error", err.message);
+        } else {
+            for(let i = 0; i < result.rows.length; i++){
+                console.log(result.rows[i].id + ":", result.rows[i].name);
+            }
+        }
+      });
+
+    });
+};
+
 let queryDoneCallback = (err, result) => {
     if (err) {
       console.log("query error", err.message);
     } else {
-      console.log("result", result.rows );
+      console.log("Information added into todo-list -->", result.rows );
     }
 };
 
@@ -25,11 +54,21 @@ let clientConnectionCallback = (err) => {
     console.log( "error", err.message );
   }
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+  let text = "INSERT INTO items (name) VALUES ($1) RETURNING *";
 
-  const values = ["hello"];
+  let inputStr = inputArr.join(' ');
+
+  const values = [inputStr];
 
   client.query(text, values, queryDoneCallback);
 };
 
-client.connect(clientConnectionCallback);
+if(input === "add"){
+    client.connect(clientConnectionCallback);
+}
+else if(input === "show"){
+    queryShowCallback();
+}
+else{
+    console.log("Error");
+}
---
+++ seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO items (name) VALUES('Go shopping');
+INSERT INTO items (name) VALUES('Feed dog');
+INSERT INTO items (name) VALUES('Swim practice');
+INSERT INTO items (name) VALUES('Code app');
+INSERT INTO items (name) VALUES('Meet gabriel');
+INSERT INTO items (name) VALUES('Eat bak kut teh');
