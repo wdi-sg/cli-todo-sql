6f031a597387fd979bb258143cc428ffea32173b
root <root@Sathvik.localdomain>
2019-01-09 07:54:15 +0800

    "Adding all the files"

---
+++ seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO items (name) VALUES ('go shopping');
+INSERT INTO items (name) VALUES ('feed dog');
+INSERT INTO items (name) VALUES ('swim practice');
+INSERT INTO items (name) VALUES ('code app');
+INSERT INTO items (name) VALUES ('meet gabriel');
