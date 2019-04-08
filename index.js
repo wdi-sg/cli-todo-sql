6734312fb4b1c55b9343ccdd8fac8bf97462dd2c
valenlyn <valenlynchua@gmail.com>
2019-04-08 22:38:18 +0800

    Added ability to delete an item

--- index.js
+++ index.js
@@ -13,6 +13,49 @@ const configs = {
 
 const client = new pg.Client(configs);
 
+//=================SHOW ITEMS=====================//
+
+function show() {
+
+    let boxDisplay = "";
+
+    const onQueryFinished = (err, result) => {
+        if (err) {
+          console.log("query error", err.message);
+        } else {
+
+          for( let i=0; i<result.rows.length; i++ ){
+
+            if (result.rows[i].done === false) {
+                boxDisplay = "[ ]";
+            } else {
+                boxDisplay = "[x]";
+            }
+
+            console.log(result.rows[i].id + ". " + boxDisplay + " – " + result.rows[i].item + " " + result.rows[i].created_at);
+          }
+        }
+    };
+
+    const onConnectServer = (err) => {
+
+        if( err ){
+            console.log( "error", err.message );
+        }
+
+        let text = "SELECT item, done, id, created_at FROM todoItems";
+
+        client.query(text, onQueryFinished);
+
+    };
+
+    client.connect(onConnectServer);
+
+}
+
+if (process.argv[2] === "show") {
+    show();
+}
 
 //==================ADD ITEM======================//
 
@@ -31,46 +74,6 @@ if (process.argv[2] === "add") {
         }
 
         let text = `INSERT INTO todoItems (item, done) VALUES ('${input}', false)`;
-
-        client.query(text, onQueryFinished);
-
-    };
-
-    client.connect(onConnectServer);
-
-}
-
-//=================SHOW ITEMS=====================//
-
-if (process.argv[2] === "show") {
-
-    let boxDisplay = "";
-
-    const onQueryFinished = (err, result) => {
-        if (err) {
-          console.log("query error", err.message);
-        } else {
-
-          for( let i=0; i<result.rows.length; i++ ){
-
-            if (result.rows[i].done === false) {
-                boxDisplay = "[ ]";
-            } else {
-                boxDisplay = "[x]";
-            }
-
-            console.log(result.rows[i].id + ". " + boxDisplay + " – " + result.rows[i].item);
-          }
-        }
-    };
-
-    const onConnectServer = (err) => {
-
-        if( err ){
-            console.log( "error", err.message );
-        }
-
-        let text = "SELECT item, done, id FROM todoItems";
 
         client.query(text, onQueryFinished);
 
@@ -111,6 +114,32 @@ if (process.argv[2] === "done") {
         }
 
         let text = `UPDATE todoItems SET done=true WHERE id=${input}`;
+
+        client.query(text, onQueryFinished);
+
+    };
+
+    client.connect(onConnectServer);
+
+}
+
+//================DELETE ITEM=====================//
+
+if (process.argv[2] === "delete") {
+
+    const onQueryFinished = (err, result) => {
+        if (err) {
+          console.log("query error", err.message);
+        }
+    };
+
+    const onConnectServer = (err) => {
+
+        if( err ){
+            console.log( "error", err.message );
+        }
+
+        let text = `DELETE from todoItems WHERE id=${input}`;
 
         client.query(text, onQueryFinished);
 
