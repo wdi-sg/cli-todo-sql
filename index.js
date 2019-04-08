26938f805fc42e941b7f815100486a71d9d74c45
claucanchin <claucanchin@gmail.com>
2019-04-09 01:08:51 +0800

    done basic mvp

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let commandType = process.argv[2];
-let activity = process.argv[3];
+const commandType = process.argv[2];
+const input = process.argv[3];
 
 const pg = require('pg');
 
@@ -12,53 +12,70 @@ const configs = {
 
 const client = new pg.Client(configs);
 
-let queryDoneCallback = (err, result) => {
-    if (err) {
-        console.log("query error", err.message);
-    } else {
-        client.query('SELECT * FROM students');
+// let queryDoneCallback = (err, result) => {
+//     if (err) {
+//         console.log("query error", err.message);
+//     } else {
+//         client.query('SELECT * FROM students');
 
-        console.log("result", result.rows);
-    }
-};
+//         console.log("result", result.rows);
+//     }
+// };
 
 let clientConnectionCallback = (err) => {
 
-    if (err) {
-        console.log("error", err.message);
-    }
+        if (err) {
+            console.log("error", err.message);
+        }
 
-    if (process.argv[2] === "add") {
+        if (commandType === "add") {
 
-        let queryText = "INSERT INTO todolist (name, donestatus) VALUES ($1, $2) RETURNING id";
+            let queryText = "INSERT INTO todolist (name, donestatus) VALUES ($1, $2) RETURNING id, name";
 
-        const values = [activity, "[ ]"];
+            const values = [input, false];
 
-        client.query(queryText, values, (err, res) => {
-            if (err) {
-                console.log("query error", err.message);
-            } else {
-                console.log("id of the thing you just created:", res.rows[0].id);
-            }
-        })
-    }
-
-    if (process.argv[2] === "show") {
-
-        let queryText = "SELECT * FROM todolist";
-
-        client.query(queryText, (err, res) => {
-            if (err) {
-                console.log("query error", err.message);
-            } else {
-                for (let i = 0; i < res.rows.length; i++) {
-                    console.log("result: ", res.rows[i]);
+            client.query(queryText, values, (err, res) => {
+                if (err) {
+                    console.log("query error", err.message);
+                } else {
+                    console.log(res.rows[0].id + ". [ ] - " + res.rows[0].name);
                 }
-            }
-        })
-    }
+            })
+        }
+
+        if (commandType === "show") {
+
+            let queryText = "SELECT * FROM todolist";
+
+            client.query(queryText, (err, res) => {
+                if (err) {
+                    console.log("query error", err.message);
+                } else {
+                    for (let i = 0; i < res.rows.length; i++) {
+                        if (res.rows[i].donestatus == false) {
+                            console.log(res.rows[i].id + ". " + "[ ]" + " - " + res.rows[i].name)
+                        } else {
+                            console.log(res.rows[i].id + ". " + "[X]" + " - " + res.rows[i].name)
+                        }
+                    }
+                }
+            })
+        }
+
+        // if (commandType === "done" && typeof(input) === 'number') {
+
+        //     let queryText = "UPDATE todolist SET donestatus=$1 WHERE id=$2";
+
+        //     const values = [true, input];
+
+        //     client.query(queryText, values, (err, res) => {
+        //         if (err) {
+        //             console.log("query error", err.message);
+        //         } else {
+        //             console.log("Marked done!");
+        //         }
+        //     })
+        // }
 
 }
-
-
-client.connect(clientConnectionCallback);
+        client.connect(clientConnectionCallback);
