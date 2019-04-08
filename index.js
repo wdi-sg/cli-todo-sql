3dd2e0faef73c13c46f5e7ea400705c9c35481db
reynosoturtle <christianreynoso96@gmail.com>
2019-04-08 22:12:01 +0800

    added add, show and mark done

--- index.js
+++ index.js
@@ -1,35 +1,107 @@
-console.log("works!!", process.argv[2]);
-
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
-    host: '127.0.0.1',
-    database: 'todo',
-    port: 5432,
+  user: 'Chris',
+  host: '127.0.0.1',
+  database: 'todo',
+  port: 5432,
 };
 
 const client = new pg.Client(configs);
 
-let queryDoneCallback = (err, result) => {
-    if (err) {
-      console.log("query error", err.message);
-    } else {
-      console.log("result", result.rows );
-    }
-};
 
-let clientConnectionCallback = (err) => {
+var add = function (newItem) {
+    const onQueryFinished = (err, result) => {
+        if (err) {
+            console.log("query error", err.message);
+        } else {
+            for( let i=0; i<result.rows.length; i++ ){
+                console.log("result: ", result.rows[i]);
+            }
+        }
+    };
+    const onConnectServer = (err) => {
 
-  if( err ){
-    console.log( "error", err.message );
-  }
+        if( err ){
+            console.log( "error", err.message );
+        }
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+        let queryText = 'INSERT INTO items (done, activity) VALUES ($1, $2) RETURNING id';
+        const values = ["undone", newItem];
 
-  const values = ["hello"];
+        client.query(queryText, values,  onQueryFinished);
 
-  client.query(text, values, queryDoneCallback);
-};
+    };
 
-client.connect(clientConnectionCallback);
+    client.connect(onConnectServer);
+}
+
+var show = function () {
+    const onQueryFinished = (err, result) => {
+        if (err) {
+            console.log("query error", err.message);
+        } else {
+         //   console.log(result)
+            for( let i=0; i<result.rows.length; i++ ){
+                if ( result.rows[i].done == "undone") {
+                    console.log( result.rows[i].id + ". [ ] - " + result.rows[i].activity );
+                } else if ( result.rows[i].done == "done") {
+                    console.log( result.rows[i].id + ". [x] - " + result.rows[i].activity );
+                }
+            }
+        }
+    };
+    const onConnectServer = (err) => {
+
+        if( err ){
+            console.log( "error", err.message );
+        }
+
+        let queryText = 'SELECT * FROM items ORDER BY id';
+
+        client.query(queryText, onQueryFinished);
+
+    };
+
+    client.connect(onConnectServer);
+
+}
+
+var done = function (itemNum) {
+
+    const onQueryFinished = (err, result) => {
+        if (err) {
+            console.log("query error", err.message);
+        } else {
+            for( let i=0; i<result.rows.length; i++ ){
+                console.log("result: ", result.rows[i]);
+            }
+        }
+    };
+    const onConnectServer = (err) => {
+
+        if( err ){
+            console.log( "error", err.message );
+        }
+
+        let queryText = "UPDATE items SET done='done' WHERE id = '"+itemNum+"';";
+
+        client.query(queryText, onQueryFinished);
+
+    };
+
+    client.connect(onConnectServer);
+
+}
+
+const calledFunction = process.argv[2];
+const userInput = process.argv[3];
+
+if (calledFunction == "add") {
+    add(userInput);
+    // console.log(obj["toDoItems"]);
+} else if (calledFunction == "show") {
+    show();
+} else if (calledFunction == "done") {
+    done(userInput);
+}
