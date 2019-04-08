9afc905665cc466aab3a1eae677decf784ae29f0
Chang <ainurlindale@gmail.com>
2019-04-08 22:36:04 +0800

    basic MVP

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("works!!", process.argv[2]);
+console.log("works!!");
 
 const pg = require('pg');
 
@@ -25,11 +25,59 @@ let clientConnectionCallback = (err) => {
     console.log( "error", err.message );
   }
 
-  let text = "INSERT INTO items (name) VALUES ($1) RETURNING id";
+  // let text = "INSERT INTO items (name) VALUES ($1) RETURNING id";
 
-  const values = ["hello"];
+  // const values = ["hello"];
 
-  client.query(text, values, queryDoneCallback);
+  if (process.argv[2] === 'show') {
+      let text = "SELECT * FROM items ORDER BY id";
+      // const values = ["hello"];
+      client.query(text, queryDoneCallback);
+  };
+
+  if (process.argv[2] === 'insert') {
+    let text = `INSERT INTO items (name) VALUES ('${process.argv[3]}');`;
+    // console.log(process.argv[3]);
+    // console.log(text);
+    client.query(text, queryDoneCallback);
+  };
+
+  if (process.argv[2] === 'update') {
+    let text = `UPDATE items SET name ='${process.argv[3]}' WHERE id = '${process.argv[4]}';`;
+    // console.log(process.argv[3]);
+    // console.log(text);
+    client.query(text, queryDoneCallback);
+  };
+
+    if (process.argv[2] === 'delete') {
+    let text = `DELETE FROM items WHERE id = '${process.argv[3]}';`;
+    // console.log(process.argv[3]);
+    // console.log(text);
+    client.query(text, queryDoneCallback);
+  };
+
+    if (process.argv[2] === 'done') {
+    let text = `UPDATE items set status ='done' WHERE id = '${process.argv[3]}';`;
+    // console.log(process.argv[3]);
+    // console.log(text);
+    client.query(text, queryDoneCallback);
+  };
+
+
+// let queryText = 'SELECT * FROM items';
+
+// client.query(queryText, (err, res) => {
+//     if (err) {
+//       console.log("query error", err.message);
+//     } else {
+//       // iterate through all of your results:
+//       for( let i=0; i<res.rows.length; i++ ){
+//         console.log("result: ", res.rows[i]);
+//       }
+//     }
+// });
+
+
 };
 
 client.connect(clientConnectionCallback);
