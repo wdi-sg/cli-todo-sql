26c03266c91f03d790c3a299f2e5a4d872976c61
seeo <aragonlotr5@gmail.com>
2019-04-08 21:06:51 +0800

    done with show, add and delete. with thanks to Sean Tan :D. May need to do bug checking later at home

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("works!!", process.argv[2]);
+// console.log("works!!", process.argv[2]);
 
 const pg = require('pg');
 
@@ -10,12 +10,16 @@ const configs = {
 };
 
 const client = new pg.Client(configs);
+let command = String(process.argv[2]);
+let activity = String(process.argv[3]);
 
 let queryDoneCallback = (err, result) => {
     if (err) {
       console.log("query error", err.message);
     } else {
-      console.log("result", result.rows );
+      for (let i = 0; i<result.rows.length;i++){
+      console.log(result.rows[i].id + ". " + result.rows[i].completion + " - " + result.rows[i].name+ " - "+result.rows[i].created_at+" - "+result.rows[i].udpated_at);
+      }
     }
 };
 
@@ -25,11 +29,59 @@ let clientConnectionCallback = (err) => {
     console.log( "error", err.message );
   }
 
-  let text = "INSERT INTO items (name) VALUES ($1) RETURNING id";
+  // let text = "SELECT * FROM items RETURNING id";
 
-  const values = ["hello"]; //process.argv[3]
+  // const values = ["hello"];
 
-  client.query(text, values, queryDoneCallback);
-};
+  // client.query(text, queryDoneCallback);
+// };
 
+  if (command === "show"){
+
+      let text = "SELECT * FROM items";
+
+      client.query(text, queryDoneCallback);
+  }
+
+  if (command === "add"){
+    let today = new Date();  //source: https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+
+    today = dd + '/' + mm + '/' + yyyy;
+
+      let text = "INSERT INTO (completion, name, created_at, updated_at) VALUES ($1, $2, $3, $4)";
+
+      let values =["[]", activity, today,"[]"]; //user should insert new item in quotes
+
+      client.query(text, values, queryDoneCallback);
+  }
+
+  if(command === "delete"){
+
+      let text = "DELETE FROM items WHERE name=$1 RETURNING *";
+
+      let values = [activity];
+
+      client.query(text, values, queryDoneCallback);
+  }
+
+// let clientConnectionCallback = (err) => {
+//
+//   if( err ){
+//     console.log( "error", err.message );
+//   }
+//
+//   let text = "INSERT INTO items (name) VALUES ($1) RETURNING id";
+//
+//   // const values = ["hello"];
+//   const values = ["process.argv[2]"];
+//
+//   client.query(text, values, queryDoneCallback);
+// };
+//
+// if (values == "show"){
+//     client.connect(clientConnectionCallback);
+}
 client.connect(clientConnectionCallback);
