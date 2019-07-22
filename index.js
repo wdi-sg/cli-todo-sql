e96aee52992e99e54e7c28c0e33633c3e6a1384f
caspianzx <caspianzx@gmail.com>
2019-07-22 20:55:29 +0800

    created function to link to psql and upload info into database

--- index.js
+++ index.js
@@ -1,35 +1,53 @@
-console.log("works!!", process.argv[2]);
-
 const pg = require('pg');
-
+//configuration
 const configs = {
-    user: 'akira',
-    host: '127.0.0.1',
-    database: 'todo',
-    port: 5432,
+user: 'caspianzx',
+host: '127.0.0.1',
+database: 'todo',
+port: 5432,
 };
+
+// const client = new pg.Client(configs);
+
+// let queryDoneCallback = (err, result) => {
+//     if (err) {
+//       console.log("query error", err.message);
+//     } else {
+//       console.log("result", result.rows );
+//     }
+// };
+
+// let clientConnectionCallback = (err) => {
+
+//   if( err ){
+//     console.log( "error", err.message );
+//   }
+
+//   let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+
+//   const values = ["hello"];
+
+//   client.query(text, values, queryDoneCallback);
+// };
+
+// client.connect(clientConnectionCallback);
+
+// let input1= process.argv[2]
 
 const client = new pg.Client(configs);
-
-let queryDoneCallback = (err, result) => {
-    if (err) {
-      console.log("query error", err.message);
-    } else {
-      console.log("result", result.rows );
-    }
-};
-
-let clientConnectionCallback = (err) => {
-
+client.connect((err) => {
   if( err ){
     console.log( "error", err.message );
-  }
-
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
-
-  const values = ["hello"];
-
-  client.query(text, values, queryDoneCallback);
-};
-
-client.connect(clientConnectionCallback);
+    } else if (process.argv[2] == 'add' && process.argv[3] != undefined) {
+        let queryText = 'INSERT INTO items (name) VALUES ($1)';
+        const values = [process.argv[3]];
+        client.query(queryText, values, (err, res) => {
+            if (err) {
+            console.log("query error", err.message);
+                } else {
+      // iterate through all of your results:
+            console.log('it works')
+            }
+        });
+    }
+});
