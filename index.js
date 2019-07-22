92ced8838f7af1816f651c455ec1ad2c8845f97f
AQILAHRAJAB <AQILAH@MAJULAH.SG>
2019-07-22 18:54:29 +0800

    Working show function

--- index.js
+++ index.js
@@ -1,35 +1,83 @@
-console.log("works!!", process.argv[2]);
 
+console.log("works!!", process.argv[2]);
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
+    user: 'nuraqilahrajab',
     host: '127.0.0.1',
     database: 'todo',
     port: 5432,
 };
 
 const client = new pg.Client(configs);
+//------------------SET UP COMPLETE----------------//
 
-let queryDoneCallback = (err, result) => {
+
+
+//-------------------SET UP FUNCTION --------------//
+
+let input2 = process.argv[2];
+let input3 = [process.argv[3]];
+let SHOW_TASKS = 'SELECT * FROM todolist';
+let ADD_TASK = 'INSERT INTO todolist (done, task) VALUES ($1, $2)';
+
+
+
+let queryDoneCallback = (err, result) => { //
     if (err) {
       console.log("query error", err.message);
     } else {
       console.log("result", result.rows );
     }
+
 };
 
-let clientConnectionCallback = (err) => {
+let clientConnectionCallback = () => {
+    switch(input2) {
+        case "show":
+        client.query(SHOW_TASKS, queryDoneCallback);
+        break;
 
-  if( err ){
-    console.log( "error", err.message );
-  }
+        case "add":
+        client.query(ADD_TASK, input3, queryDoneCallback);
+        break;
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+    }
+}
 
-  const values = ["hello"];
+client.connect(clientConnectionCallback);// inside can be function
 
-  client.query(text, values, queryDoneCallback);
-};
 
-client.connect(clientConnectionCallback);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //-------------------------------------------//
+// let queryDoneCallback = (err, result) => { //
+//     if (err) {
+//       console.log("query error", err.message);
+//     } else {
+//       console.log("result", result.rows );
+//     }
+
+// };
+
+
+
+
+  // const values = ["hello"];
+
+  // client.query(text, values, queryDoneCallback);
