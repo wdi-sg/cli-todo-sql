ca1be1c47c812cf0e68d5dec27a9d4733a5c859d
aliciawongg <w.alicia@gmail.com>
2019-07-22 23:55:52 +0800

    amend console log to show tasks

--- index.js
+++ index.js
@@ -15,8 +15,18 @@ let queryDoneCallback = (err, result) => {
     if (err) {
       console.log("query error", err.message);
     } else {
-      console.log("result", result.rows );
+        for (i=0; i<result.rows.length ; i++) {
+            console.log(`${i+1}. ID:${result.rows[i].id} ${result.rows[i].done} - ${result.rows[i].name}`);
+        }
     }
+};
+
+let queryDoneCallback2 = (err, result) => {
+    if (err) {
+      console.log("query error", err.message);
+    } else {
+        console.log("result: ", result.rows);
+        }
 };
 
 let clientConnectionCallback = (err) => {
@@ -26,11 +36,11 @@ let clientConnectionCallback = (err) => {
     }
 // query to add to list
     else if (process.argv[2] === 'add') {
-        let addTask = "INSERT INTO items (name) VALUES ($1) RETURNING id";
+        let addTask = "INSERT INTO items (name, done) VALUES ($1, '[ ]') RETURNING id";
         let newTask = process.argv[3];
         const values = newTask.split();
 
-        client.query(addTask, values, queryDoneCallback);
+        client.query(addTask, values, queryDoneCallback2);
 
         let showList = "SELECT * FROM items";
         client.query(showList, queryDoneCallback);
@@ -45,8 +55,8 @@ let clientConnectionCallback = (err) => {
     else if (process.argv[2] === 'done') {
         let taskDoneId = process.argv[3];
         console.log('completed task ' + taskDoneId);
-        let taskDone = "UPDATE items SET done='✅' WHERE id="+taskDoneId;
-        client.query(taskDone, queryDoneCallback);
+        let taskDone = "UPDATE items SET done='[X]' WHERE id="+taskDoneId;
+        client.query(taskDone, queryDoneCallback2);
 
         let showList = "SELECT * FROM items";
         client.query(showList, queryDoneCallback);
@@ -57,7 +67,7 @@ let clientConnectionCallback = (err) => {
         let taskDeleteId = process.argv[3];
         console.log('deleting task ' + taskDeleteId);
         let taskDelete = "DELETE FROM items WHERE id="+taskDeleteId;
-        client.query(taskDelete, queryDoneCallback);
+        client.query(taskDelete, queryDoneCallback2);
 
         let showList = "SELECT * FROM items";
         client.query(showList, queryDoneCallback);
