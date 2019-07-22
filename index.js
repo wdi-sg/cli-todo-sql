d9a03181fbb539deba85a923c43c9072aa39ab47
khyreerusydi <nicoraventbh@gmail.com>
2019-07-22 18:08:02 +0800

    added add, updated how client gets connected

--- index.js
+++ index.js
@@ -10,48 +10,65 @@ const configs = {
 };
 
 const commandType = process.argv[2];
-const userInput = process.argv[3]
+const userInput = process.argv[3];
 
 const client = new pg.Client(configs);
 
 // function declarations
 const show = () => {
-    client.connect((err)=>{
-        if( err ){
-            console.log( "error", err.message );
-        }
 
-        let queryString = "select * from items";
+    let queryString = "select * from items";
 
-        client.query(queryString, (err, result) => {
-            if (err) {
-              console.log("query error", err.message);
-            } else {
-                console.log(`
+    client.query(queryString, (err, result) => {
+        if (err) {
+          console.log("query error", err.message);
+        } else {
+            console.log(`
 Your list of items to do:
-                    `);
+                `);
 
-                result.rows.forEach((item, index) => {
-                    if (item.done) {
-                        console.log(`${index+1}. [X] - ${item.name}`);
-                    } else {
-                        console.log(`${index+1}. [ ] - ${item.name}`);
-                    }
-                });
+            result.rows.forEach((item, index) => {
+                if (item.done) {
+                    console.log(`${index+1}. [X] - ${item.name}`);
+                } else {
+                    console.log(`${index+1}. [ ] - ${item.name}`);
+                }
+            });
 
-                process.exit();
-            }
-        });
+            process.exit();
+        }
+    });
+}
+
+const add = () => {
+
+    let queryString = "insert into items (name, completed) values ($1, $2);";
+
+    let values = [userInput, false];
+
+    client.query(queryString, values, (err, result) => {
+        if (err) {
+          console.log("query error", err.message);
+        } else {
+            show();
+        }
     });
 }
 
 // default
-switch(commandType) {
-    case "show":
-        show();
-        break;
-    default:
-            console.log(`
+client.connect((err)=>{
+    if( err ){
+        console.log( "error", err.message );
+    } else {
+        switch(commandType) {
+            case "show":
+                show();
+                break;
+            case "add":
+                add();
+                break;
+            default:
+                    console.log(`
 Welcome to your Todo List.
 
 Commands available:
@@ -61,4 +78,6 @@ add  : creates new list items            [ node index.js add "boil water" ]
 done : marks list item as completed      [ node index.js done 2           ]
 del  : deletes list item                 [ node index.js del  1           ]
 `);
-}
+        }
+    }
+});
