8f962dbc769f8343a69233648ab79db2eba6522a
aliciawongg <w.alicia@gmail.com>
2019-07-22 17:03:28 +0800

    add functions for show all and add new tasks

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-console.log("works!!", process.argv[2]);
+console.log("starting");
 
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
+    user: 'aliciawong',
     host: '127.0.0.1',
     database: 'todo',
     port: 5432,
@@ -21,15 +21,34 @@ let queryDoneCallback = (err, result) => {
 
 let clientConnectionCallback = (err) => {
 
-  if( err ){
-    console.log( "error", err.message );
-  }
+    if( err ){
+        console.log( "error", err.message );
+    }
+// query to add to list
+    else if (process.argv[2] === 'add') {
+        let addTask = "INSERT INTO items (name) VALUES ($1) RETURNING id";
+        let newTask = process.argv[3];
+        const values = newTask.split();
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+        client.query(addTask, values, queryDoneCallback);
 
-  const values = ["hello"];
+        let showList = "SELECT * FROM items";
+        client.query(showList, queryDoneCallback);
+    }
+// query to show list
+    else if (process.argv[2] === 'show') {
+        let showList = "SELECT * FROM items";
+        client.query(showList, queryDoneCallback);
 
-  client.query(text, values, queryDoneCallback);
-};
+    }
+//query to mark task completed
+    // else if (process.argv[2] === 'done') {
+    //     let showList = "SELECT * FROM items";
+    //     client.query(showList, queryDoneCallback);
+
+    // }
+
+
+}
 
 client.connect(clientConnectionCallback);
