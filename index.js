19f56db8bc10b2bbd0118ca5d7cfae278614c9f0
samboey <samuelkjboey@gmail.com>
2019-07-23 09:07:35 +0800

    first attempt

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("works!!", process.argv[2]);
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
+    user: 'sam',
     host: '127.0.0.1',
     database: 'todo',
     port: 5432,
@@ -11,25 +11,46 @@ const configs = {
 
 const client = new pg.Client(configs);
 
+
 let queryDoneCallback = (err, result) => {
     if (err) {
       console.log("query error", err.message);
     } else {
-      console.log("result", result.rows );
+      console.log("result", result.rows[0] );
     }
 };
 
-let clientConnectionCallback = (err) => {
+// let clientConnectionCallback = (err) => {
 
-  if( err ){
-    console.log( "error", err.message );
-  }
+//   if( err ){
+//     console.log( "error", err.message );
+//   }
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
 
-  const values = ["hello"];
+//   client.query(text, values, queryDoneCallback);
+// };
 
-  client.query(text, values, queryDoneCallback);
-};
 
-client.connect(clientConnectionCallback);
+// client.connect(clientConnectionCallback);
+
+  // let text = "INSERT INTO items (name) VALUES ($1) RETURNING id";
+
+// const values = [process.argv[3]];
+let command = process.argv[2];
+
+const show = function (){
+    if (command === 'show') {
+    let queryText = 'SELECT * FROM items';
+
+    client.query(queryText,(err,res) => {
+        if (err){
+            console.log("query error", err.message);
+        }else{
+            for (let i=0; i<res.rows.length; i++){
+            console.log(res.rows[i]);
+        }
+        process.exit();
+        }
+        })
+    };
+}
