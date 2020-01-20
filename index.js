b8a44105523fcfee32425f9a7b09ac6dca22ec8d
“hweechieh” <hweechieh@gmail.com>
2020-01-20 17:24:58 +0800

    Level One completed

--- index.js
+++ index.js
@@ -3,34 +3,52 @@ console.log("works!!", process.argv[2]);
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
+    user: 'hwee',
     host: '127.0.0.1',
     database: 'todo',
     port: 5432,
 };
 
+
+
 const client = new pg.Client(configs);
 
 let queryDoneCallback = (err, result) => {
+
     if (err) {
       console.log("query error", err.message);
-    } else {
-      console.log("result", result.rows );
     }
+
+    else {
+        // console.log(result) - this is an array of objects
+      for (let i = 0; i < result.rows.length; i++) {
+      console.log(result.rows[i].id + ' [ ] - ' + result.rows[i].name);
+      }
+    }
+
     client.end();
+
 };
+
+
 
 let clientConnectionCallback = (err) => {
 
-  if( err ){
+  if (err) {
     console.log( "error", err.message );
   }
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+    let actionWord = process.argv[2];
+    let task = process.argv[3];
 
-  const values = ["hello"];
+  if (actionWord === "add") {
 
-  client.query(text, values, queryDoneCallback);
+    let queryString = "INSERT INTO items (name) VALUES ($1) RETURNING *";
+    const values = [task];
+
+    client.query(queryString, values, queryDoneCallback);
+
+  }
 };
 
 client.connect(clientConnectionCallback);
