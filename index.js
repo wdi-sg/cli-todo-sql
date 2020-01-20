82381a5f0f909ca03a542650120438458babf817
eunicelok <eunicelp511@gmail.com>
2020-01-20 23:46:28 +0800

    Able to mark as done

--- index.js
+++ index.js
@@ -15,11 +15,17 @@ let commandType = process.argv[2];
 let toDoItem = process.argv[3];
 let parseItem = parseInt(toDoItem);
 
+
+/*****************
+┌─┐┬ ┬┌─┐┬ ┬
+└─┐├─┤│ ││││
+└─┘┴ ┴└─┘└┴┘
+*****************/
+
+//this is in a way for you to see what will be log in terminal, can try console.log result to see
 const listItem = () => {
-    // console.log("not here");
     let listText = 'SELECT * FROM items ORDER BY id';
     client.query(listText, (err, result) => {
-        // console.log("after line 21");
         if (err) {
             console.log("Error!");
             client.end();
@@ -29,6 +35,14 @@ const listItem = () => {
     })
 };
 
+
+/*****************************************************
+____ _  _ ____ ____ _   _    ___  ____ _  _ ____
+|  | |  | |___ |__/  \_/     |  \ |  | |\ | |___
+|_\| |__| |___ |  \   |      |__/ |__| | \| |___
+
+******************************************************/
+
 let queryDoneCallback = (err, result) => {
     if (err) {
       console.log("query error", err.message);
@@ -36,24 +50,97 @@ let queryDoneCallback = (err, result) => {
       console.log("result", result.rows );
       listItem();
     }
-    // console.log("vaaaaaa");
 };
+
+/*****************
+____ ___  ___
+|__| |  \ |  \
+|  | |__/ |__/
+
+*****************/
+
+//TO ADD ITEMS INTO TO DO LIST
+const addItems = () => {
+    if (commandType === "add") {
+        let text = 'INSERT INTO items (name) VALUES ($1) RETURNING *';
+        const values = [toDoItem];
+        client.query(text, values, queryDoneCallback);
+    }
+    // client.end();
+};
+
+
+/***********************************
+
+___  ____ _  _ ____    _ ___ ____ _  _
+|  \ |  | |\ | |___    |  |  |___ |\/|
+|__/ |__| | \| |___    |  |  |___ |  |
+
+
+***********************************/
+
+//DONE ITEM BY MARKING [X]
+//add a column to psql for user to update have they do the list
+//https://www.postgresqltutorial.com/postgresql-add-column/
+//https://www.w3schools.com/sql/sql_update.asp
+
+//to see what has been mark as done for the chosen task in terminal
+let doneTask = (err, result) => {
+  if (err) {
+    console.log("query error", err.message);
+  } else {
+    //TODO: CONSOLE.LOG LIST OF ALL ITEMS WITH DONE ITEMS BEING MARK WITH X, OTHER'S [] REMAINS EMPTY
+    console.log(result); //  rows: [ { id: 18, name: 'run around', isdone: true } ], so have to result.rows[0]~~~~~~~~~~~~~
+    console.log(result.rows[0].id + '. [x] - '  + result.rows[0].name);
+  }
+  client.end();
+};
+
+const doneItem = () => {
+    if (commandType === "done") {
+        let text = 'UPDATE items SET isDone=TRUE WHERE id = ($1) RETURNING *';
+        const values = [parseItem];
+        client.query(text, values, doneTask);
+    }
+};
+
+
+
+/****************************************************************
+___ ____    ___  ____    ____ ____ _  _ _  _ ____ _  _ ___  ____
+ |  |  |    |  \ |  |    |    |  | |\/| |\/| |__| |\ | |  \ [__
+ |  |__|    |__/ |__|    |___ |__| |  | |  | |  | | \| |__/ ___]
+
+******************************************************************/
+
+//SETTING THE CONDITIONAL TO DO DIFFERENT FUNCTION FOR DIFFERENT COMMANDS
+const toDoCommands = () => {
+    if (commandType === "add") {
+        addItems();
+    } else if (commandType === "show") {
+        listItem();
+    } else if (commandType === "done") {
+        doneItem();
+    }    else {
+        console.log("Not a valid command!!!");
+    }
+};
+
+/***********************************************
+____ ____ _  _ _  _ ____ ____ ___
+|    |  | |\ | |\ | |___ |     |
+|___ |__| | \| | \| |___ |___  |
+
+************************************************/
 
 let clientConnectionCallback = (err) => {
     if( err ){
     console.log( "error", err.message );
     }
-  // let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
-
-  // const values = ["hello"];
-    if (commandType === "add") {
-        let text = 'INSERT INTO items (name) VALUES ($1) RETURNING *';
-        const values = [toDoItem];
-        client.query(text, values, queryDoneCallback);
-    } else if (commandType === "show") {
-            listItem();
+    if (commandType !== " ") {
+        toDoCommands();
     } else {
-        console.log("Not a valid command!");
+        console.log("You probably should check the commands.");
     }
 };
 
