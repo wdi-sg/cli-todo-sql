9c2f12f5f367a47d9b430885014586fcf7e64d8e
Stuart Myers <stuart.myers@gmail.com>
2020-01-20 16:30:40 +0800

    Did further (Can archive)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const pg = require('pg');
 const Table = require('ascii-art-table');
 const AsciiTable = require('ascii-table');
 
-console.log("works!!", process.argv[2]);
+// console.log("works!!", process.argv[2]);
 
 const configs = {
     user: 'stuartmyers',
@@ -26,37 +26,52 @@ let queryDoneCallback = (err, result) => {
         console.log("query error", err.message);
         client.end();
     } else {
-        console.log("Done!", result.rows);
-        if (result.rows[0]) {
-        let markedAsDone = result.rows[0].isdone ? "X" : " ";
-        const dateCreated = new Date(result.rows[0].datecreated);
-        const dateUpdated = new Date(result.rows[0].dateupdated);
-        console.log(`${result.rows[0].id}. [${markedAsDone}] - ${result.rows[0].name} - Created: ${dateCreated.getDate()}-${dateCreated.getMonth()+1}-${dateCreated.getFullYear()} - Updated: ${dateUpdated.getDate()}-${dateUpdated.getMonth()+1}-${dateUpdated.getFullYear()}`);
+        // console.log("Done!", result.rows);
+        if (result.rows[0] && !result.rows[0].archived) {
+            let markedAsDone = result.rows[0].isdone ? "X" : " ";
+            const dateCreated = new Date(result.rows[0].datecreated);
+            const dateUpdated = new Date(result.rows[0].dateupdated);
+            console.log(`${result.rows[0].id}. [${markedAsDone}] - ${result.rows[0].name} - Created: ${dateCreated.getDate()}-${dateCreated.getMonth()+1}-${dateCreated.getFullYear()} - Updated: ${dateUpdated.getDate()}-${dateUpdated.getMonth()+1}-${dateUpdated.getFullYear()}`);
         }
         console.log('****************************************')
         listToDoListItems();
     }
 };
 
+// Archive an ID (not delete)
+const archiveItem = (idNo) => {
+    const inputNo = parseInt(idNo);
+    // TODO: TEST IF The index exists in the DB.
+    if (isNaN(idNo)) {
+        console.log('Error please put in a valid number');
+        return;
+    }
+    let text = "UPDATE items SET archived=$1, dateupdated=$2 WHERE id=$3 RETURNING *"
+    const date = new Date();
+    const values = [true, date, inputNo];
+    client.query(text, values, queryDoneCallback);
+}
+
 // List the items.
 const listToDoListItems = () => {
-  let listText = "SELECT * FROM items ORDER BY id";
-  client.query(listText, (err, result) => {
-    let displayTable = new AsciiTable('TO-DO LIST');
-    displayTable.setHeading('No.', 'Done', 'Item', 'Date Created', 'Date Updated')
-    displayTable.setAlign(1, AsciiTable.CENTER);
-    for (let i = 0; i < result.rows.length; i++) {
-        let markedAsDone = result.rows[i].isdone ? "X" : " ";
-        const dateCreated = new Date(result.rows[i].datecreated);
-        const dateCreatedString = `${dateCreated.getDate().toString().padStart(2,'0')}-${(dateCreated.getMonth()+1).toString().padStart(2,'0')}-${dateCreated.getFullYear()}`;
-        const dateUpdated = new Date(result.rows[i].dateupdated);
-        const dateUpdatedString = `${(dateUpdated.getDate()).toString().padStart(2,'0')}-${(dateUpdated.getMonth()+1).toString().padStart(2,'0')}-${dateUpdated.getFullYear()}`;
-        // console.log(`${result.rows[i].id}. [${markedAsDone}] - ${result.rows[i].name} - Created: ${dateCreated.getDate()}-${dateCreated.getMonth()+1}-${dateCreated.getFullYear()} - Updated: ${dateUpdated.getDate()}-${dateUpdated.getMonth()+1}-${dateUpdated.getFullYear()}`);
-    displayTable.addRow(result.rows[i].id, markedAsDone, result.rows[i].name ,dateCreatedString, dateUpdatedString);
-    }
-    console.log(displayTable.toString());
-    client.end();
-  })
+    let listText = "SELECT * FROM items ORDER BY id";
+    client.query(listText, (err, result) => {
+        let displayTable = new AsciiTable('TO-DO LIST');
+        displayTable.setHeading('No.', 'Done', 'Item', 'Date Created', 'Date Updated')
+        displayTable.setAlign(1, AsciiTable.CENTER);
+        for (let i = 0; i < result.rows.length; i++) {
+            if (result.rows[i].archived) {continue};
+            let markedAsDone = result.rows[i].isdone ? "X" : " ";
+            const dateCreated = new Date(result.rows[i].datecreated);
+            const dateCreatedString = `${dateCreated.getDate().toString().padStart(2,'0')}-${(dateCreated.getMonth()+1).toString().padStart(2,'0')}-${dateCreated.getFullYear()}`;
+            const dateUpdated = new Date(result.rows[i].dateupdated);
+            const dateUpdatedString = `${(dateUpdated.getDate()).toString().padStart(2,'0')}-${(dateUpdated.getMonth()+1).toString().padStart(2,'0')}-${dateUpdated.getFullYear()}`;
+            // console.log(`${result.rows[i].id}. [${markedAsDone}] - ${result.rows[i].name} - Created: ${dateCreated.getDate()}-${dateCreated.getMonth()+1}-${dateCreated.getFullYear()} - Updated: ${dateUpdated.getDate()}-${dateUpdated.getMonth()+1}-${dateUpdated.getFullYear()}`);
+            displayTable.addRow(result.rows[i].id, markedAsDone, result.rows[i].name, dateCreatedString, dateUpdatedString);
+        }
+        console.log(displayTable.toString());
+        client.end();
+    })
 
 }
 
@@ -109,7 +124,7 @@ let clientConnectionCallback = (err) => {
     if (process.argv[2]) {
         whatCommand();
     } else {
-      listToDoListItems();
+        listToDoListItems();
         // let text = "SELECT * FROM items";
         //
         // client.query(text, queryDoneCallback);
@@ -136,6 +151,9 @@ const whatCommand = () => {
             break;
         case 'delete':
             deleteItem(commandArg);
+            break;
+        case 'archive':
+            archiveItem(commandArg);
             break;
         default:
             console.log('Not a valid command');
