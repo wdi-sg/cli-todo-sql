c7e468a2b0336418dc650cd7999c5a10ab84acdf
weizheng1910 <weizheng1910@gmail.com>
2020-01-20 16:12:38 +0800

    can add task and show current to-dos

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("works!!", process.argv[2]);
 
 const pg = require('pg');
 
@@ -11,11 +10,35 @@ const configs = {
 
 const client = new pg.Client(configs);
 
+// External Functions
+
+function showDoneYet(doneYet) {
+  if(doneYet == false){
+    return "[ ]"
+  } else {
+    return "[X]"
+  }
+}
+
+//`${result.rows.id}. ${showDoneYet(result.rows.doneYet)} ${result.rows.name}`
+// End External Functions
+
 let queryDoneCallback = (err, result) => {
     if (err) {
       console.log("query error", err.message);
     } else {
-      console.log("Result: ", result.rows );
+      console.log("Task inserted!");
+      console.log("Current data: ")
+    }
+};
+
+let queryDoneCallback2 = (err, result) => {
+    if (err) {
+      console.log("query error", err.message);
+    } else {
+      for(let i = 0; i < result.rows.length; i++){
+        console.log(`${i + 1}. ${showDoneYet(result.rows[i].doneyet)} ${result.rows[i].name}`)
+      }
     }
     client.end();
 };
@@ -26,12 +49,21 @@ let clientConnectionCallback = (err) => {
     console.log( "error", err.message );
   }
 
-  let newTask = process.argv[2];
+  let newTask = process.argv[3];
+
+  // Query Strings 
   let text = "INSERT INTO items (name,doneYet) VALUES ($1,$2) RETURNING *";
+  let text2 = "SELECT * from items"
 
   const values = [newTask,false];
 
-  client.query(text, values, queryDoneCallback);
+  if(process.argv[2] == "show"){
+    client.query(text2,queryDoneCallback2)
+  } else if (process.argv[2] == "add") {
+    client.query(text, values, queryDoneCallback);
+    client.query(text2,queryDoneCallback2)
+  }
+
   
 };
 
