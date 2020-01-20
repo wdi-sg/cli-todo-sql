fe28c383243bbecc5c63e3d01b72c8de6d7a086c
Angel Ferreros <AngelFerreros@TJs-MacBook-Pro.local>
2020-01-20 15:44:52 +0800

    Able to add task in database server.

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-console.log("works!!", process.argv[2]);
+console.log("works!!", process.argv[3]);
 
+//configs
 const pg = require('pg');
-
 const configs = {
-    user: 'akira',
+    user: 'AngelFerreros',
     host: '127.0.0.1',
     database: 'todo',
     port: 5432,
 };
 
 const client = new pg.Client(configs);
+//end of configs
+
 
 let queryDoneCallback = (err, result) => {
     if (err) {
       console.log("query error", err.message);
     } else {
       console.log("result", result.rows );
+      //NOTE: result.rows = array of objects
+      console.log("result", result.rows[0].name );
     }
-    client.end();
+    client.end(); //disconnects client from DB Server
 };
 
+//function that runs once client is connected to server
 let clientConnectionCallback = (err) => {
-
   if( err ){
     console.log( "error", err.message );
   }
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+  let text = "INSERT INTO items (name) VALUES ($1) RETURNING *";
+  const values = process.argv[3];
+    let taskToPrint = [`[ ] - `+ values];
+    console.log(taskToPrint);
 
-  const values = ["hello"];
-
-  client.query(text, values, queryDoneCallback);
+  client.query(text, taskToPrint, queryDoneCallback);
 };
 
 client.connect(clientConnectionCallback);
