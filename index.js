5364b9c09395e77daf088c91f362ac1f1470ce2a
SamTanFL <samsite_tan@hotmail.com>
2020-01-20 20:44:46 +0800

    added skeleton functions

--- index.js
+++ index.js
@@ -1,36 +1,59 @@
-console.log("works!!", process.argv[2]);
-
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
-    host: '127.0.0.1',
-    database: 'todo',
-    port: 5432,
+        user: 'postgres',
+        host: '127.0.0.1',
+        database: 'todo',
+        port: 5432,
 };
+
 
 const client = new pg.Client(configs);
 
+
 let queryDoneCallback = (err, result) => {
-    if (err) {
-      console.log("query error", err.message);
-    } else {
-      console.log("result", result.rows );
-    }
-    client.end();
+        if (err) {
+            console.log("query error", err.message);
+        }
+        switch (process.argv[2].toLowerCase()) {
+            case "show":
+                console.log("result", result.rows );
+            break;
+            case "add":
+                console.log("Added $1 to To-Do-List")
+            break;
+            case "done":
+                console.log(result.rows[0].name + " task done.")
+            break;
+            default:
+            console.log("Something is wrong in queryDoneCallback")
+        }
+        client.end();
 };
 
+
 let clientConnectionCallback = (err) => {
-
-  if( err ){
-    console.log( "error", err.message );
-  }
-
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
-
-  const values = ["hello"];
-
-  client.query(text, values, queryDoneCallback);
+    if( err ){
+        console.log( "error", err.message );
+    }
+let text;
+let values = [];
+    switch (process.argv[2].toLowerCase()) {
+        case "show":
+            text = "SELECT * FROM items ORDER BY id ASC";
+        break;
+        case "add":
+            text = "INSERT INTO items (name, done) VALUES ($1, false) RETURNING *";
+            values = [process.argv[3]];
+        break;
+        case "done":
+            text = "UPDATE items SET done=true WHERE id=$1 RETURNING *"
+            values = process.argv[3];
+        break;
+        default:
+        console.log("Something is wrong")
+    }
+    client.query(text, values, queryDoneCallback);
 };
 
 client.connect(clientConnectionCallback);
