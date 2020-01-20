5aed7bbada3ac81eb4bb77dc4e4f4ae42bf74b0e
weizheng1910 <weizheng1910@gmail.com>
2020-01-20 15:23:35 +0800

    can add task

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("works!!", process.argv[2]);
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
+    user: 'weizheng1910',
     host: '127.0.0.1',
     database: 'todo',
     port: 5432,
@@ -15,7 +15,7 @@ let queryDoneCallback = (err, result) => {
     if (err) {
       console.log("query error", err.message);
     } else {
-      console.log("result", result.rows );
+      console.log("Result: ", result.rows );
     }
     client.end();
 };
@@ -26,11 +26,13 @@ let clientConnectionCallback = (err) => {
     console.log( "error", err.message );
   }
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+  let newTask = process.argv[2];
+  let text = "INSERT INTO items (name,doneYet) VALUES ($1,$2) RETURNING *";
 
-  const values = ["hello"];
+  const values = [newTask,false];
 
   client.query(text, values, queryDoneCallback);
+  
 };
 
 client.connect(clientConnectionCallback);
