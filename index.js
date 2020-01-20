cfd54eee367d485aad5905ea2b8dd3280c8f8e21
joycemap <57893220+joycemap@users.noreply.github.com>
2020-01-20 18:16:08 +0800

    Show table

--- index.js
+++ index.js
@@ -19,8 +19,23 @@ let clientConnectionCallback = (err) => {
   if( err ){
     console.log( "error", err.message );
   }
-  let text = "INSERT INTO items (name) VALUES ($1) RETURNING *";
-  const values = [process.argv[3]];
-  client.query(text, values, queryDoneCallback);
-};
+      if (process.argv[2] === "show"){
+        let text1 = 'SELECT * FROM items';
+        client.query(text1, (err, res) => {
+            if (err) {
+              console.log("query error", err.message);
+            } else {
+              // iterate through all of your results:
+              for( let i=0; i<res.rows.length; i++ ){
+                console.log("result: ", res.rows[i]);
+              }
+            }
+        });
+      }
+      else (process.argv[2]==="add")
+        let text2 = "INSERT INTO items (name) VALUES ($1) RETURNING *";
+        const values = [process.argv[3]];
+        client.query(text2, values, queryDoneCallback);
+      };
+
 client.connect(clientConnectionCallback);
