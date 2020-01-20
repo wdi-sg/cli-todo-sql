cb4989f4302e083ad3796ecfdb2bb91c9abe7c27
benjacoblee <benjacoblee@gmail.com>
2020-01-20 16:48:27 +0800

    update date when mark as done

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log("works!!", process.argv[2]);
-
 const pg = require("pg");
 
 const configs = {
@@ -9,15 +7,58 @@ const configs = {
   port: 5432
 };
 
+const createTableText = `
+CREATE TEMP TABLE dates(
+  date_col DATE DEFAULT now(),
+  timestamp_col TIMESTAMP DEFAULT now(),
+  timestamptz_col TIMESTAMPTZ DEFAULT now(),
+  created_at TIMESTAMPTZ DEFAULT now()
+);
+`;
+
 const action = process.argv[2];
+let todo = process.argv[3];
 
 const client = new pg.Client(configs);
 
-let queryDoneCallback = (err, result) => {
+// let queryDoneCallback = (err, result) => {
+//   if (err) {
+//     console.log("query error", err.message);
+//   } else {
+//     console.log("result", result.rows);
+//   }
+//   client.end();
+// };
+
+let showTodos = (err, result) => {
+  for (let i = 0; i < result.rows.length; i++) {
+    let todo = result.rows[i];
+    let done = todo.done;
+    if (done === true) {
+      console.log(`${todo.id}. [x] - ${todo.name} 
+      Created at: ${todo.created_at},
+      Updated at ${todo.updated_at}`);
+    } else {
+      console.log(`${todo.id}. [ ] - ${todo.name} 
+      Created at: ${todo.created_at}
+      Updated at ${todo.updated_at}`);
+    }
+  }
+  client.end();
+};
+
+let addTodo = (err, result) => {
+  const todoName = result.rows[0].name;
+  console.log(`Added todo: ${todoName}`);
+  client.end();
+};
+
+let doneTodo = (err, result) => {
   if (err) {
     console.log("query error", err.message);
   } else {
-    console.log("result", result.rows);
+    console.log(`${result.rows[0].id}. [x] - ${result.rows[0].name} 
+    Updated at ${result.rows[0].updated_at}`);
   }
   client.end();
 };
@@ -28,16 +69,19 @@ let clientConnectionCallback = err => {
   }
 
   if (action === "show") {
-    text = "SELECT * from items";
-    client.query(text, queryDoneCallback);
+    text = "SELECT * from items order by id";
+    client.query(text, showTodos);
   } else if (action === "add") {
-    let todo = "[]" + process.argv[3];
-    text = `INSERT INTO items (name) VALUES ('${todo}') RETURNING id`;
-    client.query(text, queryDoneCallback);
+    text = `INSERT INTO items (name) VALUES ('${todo}') RETURNING name`;
+    client.query(text, addTodo);
+  } else if (action === "done") {
+    text = `UPDATE items 
+    SET done = TRUE, updated_at = NOW()
+    WHERE id = ${process.argv[3]} RETURNING *;`;
+    client.query(text, doneTodo);
   }
 
   // const values = ["hello"];
-
 };
 
 client.connect(clientConnectionCallback);
