964994b341ec609b92d746617ce4cb73d0dff154
kwansing14 <kwansing@hotmail.sg>
2020-04-04 11:44:56 +0800

    did up to average completion time

--- index.js
+++ index.js
@@ -1,36 +1,131 @@
-console.log("works!!", process.argv[2]);
+const jsonfile = require('jsonfile');
+const { DateTime } = require("luxon");
+DateTime.local();
+const { Interval } = require("luxon");
+const { Duration }= require("luxon");
 
-const pg = require('pg');
 
-const configs = {
-    user: 'akira',
-    host: '127.0.0.1',
-    database: 'todo',
-    port: 5432,
-};
+const file = 'data.json'
 
-const client = new pg.Client(configs);
+let newKey = process.argv[2]
+let newValue = process.argv[3]
+let nDate;
+let dt;
+let nMilli = 0;
+let timeTotal = 0;
+let averageTime = 0;
+let counter = 0;
 
-let queryDoneCallback = (err, result) => {
-    if (err) {
-      console.log("query error", err.message);
-    } else {
-      console.log("result", result.rows );
+jsonfile.readFile(file, (err, obj) => {
+    //----------------------------------show
+    if( process.argv[2] == "show") {
+
+        console.log(obj)
+
+        for (let i=0 ; i<obj.todoItems.length ; i++){
+
+            let x = i+1;
+            console.log(x+". "+obj.brac[i]+" - "+obj.todoItems[i]+" - "+obj.date[i]);
+        }
     }
-    client.end();
-};
+    //------------------------------------clear
+    if (process.argv[2] == "clear") {
 
-let clientConnectionCallback = (err) => {
+        obj.brac = [];
+        obj.todoItems = [];
+        obj.date = [];
+        obj.diff = [];
+        console.log(obj)
+    }
+    //-------------------------------------add
+    if (process.argv[2] == "add") {
+        if (obj.todoItems == undefined) {
+            obj.brac = [];
+            obj.todoItems = [];
+            obj.date = [];
+            obj.diff = [];
 
-  if( err ){
-    console.log( "error", err.message );
-  }
+            createDate();
+            obj.brac.push("[ ]");
+            obj.todoItems.push(process.argv[3]);
+            obj.date.push(nDate);
+            obj.diff.push(nMilli);
+        }
+        else {
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+            createDate();
+            obj.brac.push("[ ]");
+            obj.todoItems.push(process.argv[3]);
+            obj.date.push(nDate);
+            obj.diff.push(nMilli);
+        }
+    }
+    //---------------------------------------done
+    if(process.argv[2] == 'done') {
+        let i = process.argv[3] - 1;
+        obj.brac[i] = "[x]"; //mark X
 
-  const values = ["hello"];
+        createDate();
+        let timeDiff = (nMilli - parseInt(obj.diff[i])); //calculate difference
+        obj.diff[i] = timeDiff; //replace with timeDifference in milliseconds
+        obj.date[i] = nDate;    //replace with new Date.
 
-  client.query(text, values, queryDoneCallback);
-};
+        console.log(obj);
+    }
 
-client.connect(clientConnectionCallback);
+    jsonfile.writeFile(file, obj, (err) => {
+        console.log(err)
+    });
+    //--------------------------------------stats
+    if(process.argv[2] == 'stats') {
+
+        if(process.argv[3] == 'complete-time') {
+            for(let i=0 ; i<obj.todoItems.length ; i++) {
+                if(obj.brac[i]="[x]") {
+                    timeTotal += obj.diff[i]; //add list that has done
+                    counter++;
+                    console.log(milliConvert(obj.diff[i])); //display all completion time
+                }
+            }
+            averageTime = timeTotal/counter;
+            averageTime = milliConvert(averageTime);  //convert average time to 0:16:11
+            console.log("Average completion time for "+counter+" items, "+averageTime)
+        }
+
+    }
+
+    jsonfile.writeFile(file, obj, (err) => {
+        console.log(err)
+    });
+});
+
+let createDate = function () {
+    dt = DateTime.local();
+    var f = {month: 'long', day: 'numeric'};
+    nDate = dt.toLocaleString(DateTime.DATETIME_MED);
+    nMilli = dt.toMillis();
+    //console.log(nMilli)
+}
+//createDate();
+
+let test = function () {
+    let i1 = DateTime.fromISO('1982-05-25T09:45');
+    let i2 = DateTime.fromISO('1983-10-14T10:30');
+    console.log(i1)
+    // console.log(typeof i1)
+    // console.log(typeof i2)
+    // console.log(i2.diff(i1).toObject()) //=> { milliseconds: 43807500000 }
+    // console.log(i2.diff(i1, 'hours').toObject()) //=> { hours: 12168.75 }
+    // console.log(i2.diff(i1, ['months', 'days']).toObject()) //=> { months: 16, days: 19.03125 }
+    // console.log(i2.diff(i1, ['months', 'days', 'hours']).toObject()) //=> { months: 16, days: 19, hours: 0.75 }
+
+}
+//test();
+
+let milliConvert = function (x) {
+    ms = parseInt(x);
+    ms = 1000*Math.round(ms/1000); // round to nearest second
+    let d = new Date(ms);
+    newMs = d.getUTCHours()+':'+d.getUTCMinutes()+':'+d.getUTCSeconds(); // "4:59"
+    return newMs;
+}
