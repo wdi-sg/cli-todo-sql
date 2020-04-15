4dfb27b2bf89940f6a398141c4aeebce72412177
Rachelik <rachel.ik.87@gmail.com>
2020-04-15 23:08:03 +0800

    IN: Show and Add function.

--- todo.js
+++ todo.js
@@ -21,16 +21,33 @@ let queryDoneCallback = (err, result) => {
 };
 
 let clientConnectionCallback = (err) => {
-
   if( err ){
     console.log( "error", err.message );
   }
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+  const actToDo = process.argv[2];
 
-  const values = ["hello"];
+  if (actToDo === "show") {
+    //to show all the list
+    let showItems = "SELECT * FROM items";
+    client.query(showItems, queryDoneCallback);
 
-  client.query(text, values, queryDoneCallback);
+  } else if (actToDo === "add") {
+    //to add item to the list
+    const notDone = '[ ]';
+    const taskDescription = process.argv[3];
+
+    let addItems = "INSERT INTO items (status, task) VALUES ($1, $2) RETURNING id";
+    const values = [notDone, taskDescription];
+
+    client.query(addItems, values, queryDoneCallback);
+  }
+
+
+  // let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+
+  // const values = ["hello"];
+
 };
 
 client.connect(clientConnectionCallback);
