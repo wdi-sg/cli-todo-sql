c98951c853cdfde884621b8feafbcdb8d3388643
YusofGotboudine <yusof95@hotmail.com>
2020-04-15 20:19:36 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ console.log("works!!", process.argv[2]);
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
-    host: '127.0.0.1',
-    database: 'todo',
-    port: 5432,
+  user: 'yusofgotboudine',
+  host: '127.0.0.1',
+  database: 'todo',
+  port: 5432,
 };
 
 const client = new pg.Client(configs);
 
 let queryDoneCallback = (err, result) => {
-    if (err) {
-      console.log("query error", err.message);
-    } else {
-      console.log("result", result.rows );
-    }
-    client.end();
+  if (err) {
+    console.log("query error", err.message);
+  } else {
+    console.log("result", result.rows);
+  }
+  client.end();
 };
 
 let clientConnectionCallback = (err) => {
 
-  if( err ){
-    console.log( "error", err.message );
+  if (err) {
+    console.log("error", err.message);
   }
 
   let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
