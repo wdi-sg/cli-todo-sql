9ffd32eec68807dc8e85d791f86aadeaf32b2358
osbornechan <osbornecab@gmail.com>
2020-04-15 19:25:22 +0800

    Added show and add functionality

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-console.log("works!!", process.argv[2]);
-
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
+    user: 'chanosborne',
     host: '127.0.0.1',
     database: 'todo',
     port: 5432,
@@ -11,26 +9,30 @@ const configs = {
 
 const client = new pg.Client(configs);
 
-let queryDoneCallback = (err, result) => {
+client.connect((err) => {
     if (err) {
-      console.log("query error", err.message);
-    } else {
-      console.log("result", result.rows );
+        console.log('connectionError', err.message);
     }
-    client.end();
-};
 
-let clientConnectionCallback = (err) => {
+    let commandType = process.argv[2];
+    let inputTask = process.argv[3];
+    let values = [];
+    let queryText;
 
-  if( err ){
-    console.log( "error", err.message );
-  }
+    if (commandType === 'show') {
+        queryText = 'SELECT * FROM items';
+    } else if (commandType === 'add') {
+        values = [inputTask];
+        queryText = "INSERT INTO items (done, task) VALUES ('[] -', $1) RETURNING *";
+    }
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
-
-  const values = ["hello"];
-
-  client.query(text, values, queryDoneCallback);
-};
-
-client.connect(clientConnectionCallback);
+    client.query(queryText, values, (err, res) => {
+        if (err) {
+            console.log('queryError', err.message)
+        } else {
+            res.rows.forEach(row => {
+                console.log(`${row.id}. ${row.done} ${row.task}`);
+            })
+        }
+    })
+});
