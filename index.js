eac6f0ba7aa0698df749aec29b5b92b7764ec464
chelsejw <chelsejw@gmail.com>
2020-04-15 19:31:40 +0800

    add function to complete items

--- index.js
+++ index.js
@@ -9,28 +9,62 @@ const configs = {
 
 const client = new pg.Client(configs);
 
+const d = new Date();
+const today = `${d.getDate()}/${(d.getMonth()+1)}/${d.getFullYear()}`
+const now = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
+
+const command = process.argv[2]
+
 let queryDoneCallback = (err, result) => {
     if (err) {
-      console.log("query error", err.message);
+        console.log("query error", err.message);
     } else {
-      console.log("result", result.rows );
+        showResults(result);
     }
     client.end();
 };
 
+
+const showResults = res => {
+    res.rows.forEach(item => {
+        let box = `[ ]`
+        item.completion ? box = `[X]` : box
+        console.log(`${item.id}. ${box} - ${item.name}`);
+    });
+};
+
+
 let clientConnectionCallback = (err) => {
 
-  if( err ){
-    console.log( "error", err.message );
-  }
+    if (err) {
+        console.log("error", err.message);
+    }
 
-  const inputs = process.argv.slice(2).join(" ");
+    if (command === 'add') {
+        const inputs = process.argv.slice(3).join(" ");
 
-  let text = `INSERT INTO items (name) VALUES ('${inputs}') RETURNING id, name`;
+        let text = `INSERT INTO items (name) VALUES ('${inputs}') RETURNING id, name`;
 
-  client.query(text, queryDoneCallback);
+        client.query(text, queryDoneCallback);
+        
+    } else if (command === 'done') {
 
+        const targetItem = process.argv[3];
+        const script = `UPDATE items SET completion = TRUE WHERE id=${targetItem}`
+        client.query(script, queryDoneCallback);
+
+    } else if (command === 'show') {
+
+        const script = `SELECT * FROM items`
+        displayList();
+
+    }
 
 };
+
+
+const displayList = ()=> {
+  client.query(`SELECT * FROM items`, queryDoneCallback);
+}
 
 client.connect(clientConnectionCallback);
