d6a2a75a5e3a1c950c045f15fabc3db64a161759
travisenquiry <travisenquiry@gmail.com>
2020-04-15 19:49:01 +0800

    completed add function

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-console.log("works!!", process.argv[2]);
-
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
+    user: 'elter',
     host: '127.0.0.1',
     database: 'todo',
     port: 5432,
@@ -11,7 +9,7 @@ const configs = {
 
 const client = new pg.Client(configs);
 
-let queryDoneCallback = (err, result) => {
+/*let queryDoneCallback = (err, result) => {
     if (err) {
       console.log("query error", err.message);
     } else {
@@ -33,4 +31,28 @@ let clientConnectionCallback = (err) => {
   client.query(text, values, queryDoneCallback);
 };
 
-client.connect(clientConnectionCallback);
+client.connect(clientConnectionCallback);*/
+
+client.connect((err) => {
+  if(err){
+    console.log("Error connecting: ", err.message);
+  }else {
+    if(process.argv[2] === "add") {
+      let task = "";
+      let date = Date();
+      for(let i=3; i<process.argv.length; i++){
+        task = task + " " + process.argv[i];
+      }
+      let values = [task, '[ ]', date.toString(), '-'];
+      let queryText = `INSERT INTO items (task, isdone, created_at, updated_at) VALUES ($1, $2, $3, $4) RETURNING *`;
+
+      client.query(queryText, values, (err, res) =>{
+        if(err){
+          console.log("Error in query: ", err.message);
+        }else {
+          console.log(`ID ${res.rows[0].id} has been inserted, result: `, res.rows);
+        }
+      });
+    }
+  }
+});
