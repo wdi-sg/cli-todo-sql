435d8007976c2e7b799dcd2894497cf0fce408d6
jordanswp <sohweepin@gmail.com>
2020-04-15 23:48:49 +0800

    done first part

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
-console.log("works!!", process.argv[2]);
+console.log("works!!", process.argv[2], process.argv[3]);
 
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
+    user: 'weepinsoh',
     host: '127.0.0.1',
     database: 'todo',
     port: 5432,
 };
+// what the user types in becomes part of the SQL query
+let userInput = process.argv[2];
+let nameInput = process.argv[3];
 
 const client = new pg.Client(configs);
 
-let queryDoneCallback = (err, result) => {
-    if (err) {
+let queryDone = (queryError, result) => {
+    if (queryError) {
       console.log("query error", err.message);
     } else {
       console.log("result", result.rows );
     }
-    client.end();
+    // client.end();
 };
 
-let clientConnectionCallback = (err) => {
+let clientConnectionCallback = (connectionError) => {
 
-  if( err ){
+  if( connectionError ){
     console.log( "error", err.message );
   }
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+  let queryText = 'INSERT INTO items (name) VALUES ($1) RETURNING *';
+  const values = [nameInput];
+if(userInput === "show"){
+      const text = 'SELECT * FROM items';
+    client.query(text, (err, res) => {
+    if (err) {
+      console.log("query error", err.message);
+    } else {
+          for( let i=0; i<res.rows.length; i++ ){
+        console.log("result: ", res.rows[i]);
+      }
+    }
+});
+} else { client.query(queryText, values, (err, res) =>{
+    if (err) {
+        console.log("query error", err.message);
+    } else {
+         //console.log("id of the thing you just created:", res.rows[0].id);
+        console.log("done");
+        console.log(res);
 
-  const values = ["hello"];
-
-  client.query(text, values, queryDoneCallback);
+    }
+});
+}
 };
 
 client.connect(clientConnectionCallback);
