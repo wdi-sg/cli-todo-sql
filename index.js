5576f58f67a4bd2cbc23175128db23806eb01e8a
wickker <choydianchun@gmail.com>
2020-04-15 17:54:00 +0800

    display todolist done

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
-console.log("works!!", process.argv[2]);
-
-const pg = require('pg');
+const pg = require("pg");
 
 const configs = {
-    user: 'akira',
-    host: '127.0.0.1',
-    database: 'todo',
-    port: 5432,
+  user: "postgres",
+  host: "127.0.0.1",
+  database: "todo",
+  port: 5432,
 };
 
 const client = new pg.Client(configs);
 
-let queryDoneCallback = (err, result) => {
-    if (err) {
-      console.log("query error", err.message);
-    } else {
-      console.log("result", result.rows );
+//------------------------------
+//------------------------------
+
+let inputArr = process.argv;
+console.log(inputArr);
+
+let queryDoneShow = (err, result) => {
+  if (err) {
+    console.log("QUERY ERROR:", err.message);
+  } else {
+    for (let i = 0; i < result.rows.length; i++) {
+      let obj = result.rows;
+      let displayText;
+      if (obj[i].done === null) {
+        displayText = `${obj[i].id}. [ ] - ${obj[i].item}`;
+      } else {
+        displayText = `${obj[i].id}. [${obj[i].done}] - ${obj[i].item}`;
+      }
+      console.log(displayText);
     }
-    client.end();
-};
-
-let clientConnectionCallback = (err) => {
-
-  if( err ){
-    console.log( "error", err.message );
   }
-
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
-
-  const values = ["hello"];
-
-  client.query(text, values, queryDoneCallback);
+  client.end();
 };
 
-client.connect(clientConnectionCallback);
+let clientConnectionCallBack = (err) => {
+  if (err) {
+    console.log("CONNECTION ERROR:", err.message);
+  } else if (inputArr[2] === "show") {
+    let queryText = "SELECT * from todolist";
+    client.query(queryText, queryDoneShow);
+  }
+};
+
+client.connect(clientConnectionCallBack);
