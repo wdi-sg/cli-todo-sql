8cb8f11fc8073af3dfb7590c2508f70c0baeccf7
Nausheen_S <salauddinnausheen@gmail.com>
2020-04-15 20:28:26 +0800

    completed first deliverable

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-console.log("works!!", process.argv[2]);
+/*console.log("works!!", process.argv[2]);
 
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
+    user: 'nausheen',
     host: '127.0.0.1',
     database: 'todo',
     port: 5432,
@@ -11,7 +11,8 @@ const configs = {
 
 const client = new pg.Client(configs);
 
-let queryDoneCallback = (err, result) => {
+
+/*let queryDoneCallback = (err, result) => {
     if (err) {
       console.log("query error", err.message);
     } else {
@@ -34,3 +35,139 @@ let clientConnectionCallback = (err) => {
 };
 
 client.connect(clientConnectionCallback);
+
+let clientConnectionCallback = (err) => {
+    let action = process.argv[2];
+    let name = process.argv[3];
+    let values;
+    if (action == "show") {
+        client.connect((err) => {
+            if( err ){
+             console.log( "error", err.message );
+            }
+            const text = 'SELECT * FROM items'
+            client.query(text, (err, res) => {
+             if (err) {
+               console.log("query error", err.message);
+             } else {
+               console.log("result", res.rows);
+               client.end();
+             }
+           });
+
+         });
+     }
+    if (action == "add"){
+    queryText = 'INSERT INTO items (name) VALUES ($1) RETURNING id'; 
+    values = [name]; 
+    client.query(queryText, values, (err, res) => {
+        if (err) {
+           console.log("query error", err.message);
+        } else {
+            console.log("created!")
+             client.end();
+        }
+    });
+}
+    else{ 
+    console.log("invalid input");
+    return;
+    }
+};
+client.connect(clientConnectionCallback); 
+
+
+
+
+
+
+
+client.connect((err) => {
+
+  if( err ){
+    console.log( "error", err.message );
+  }
+
+
+  let action = process.argv[2];
+  let name = process.argv[3];
+  let values;
+  if (action == "add"){
+     values = [name]; 
+    queryText = 'INSERT INTO items (name) VALUES ($1) RETURNING id'; 
+    } else if (process.argv[2] == "show") {
+        values = [name]
+       queryText = "SELECT * FROM items RETURNING id"; 
+    } else if (process.argv[2] == "done") { 
+       values = [name]; 
+       queryText = "UPDATE items SET name = '[X]' WHERE name = $1 RETURNING id"; 
+
+} else
+{ 
+    console.log("invalid input");
+    return;
+} 
+
+  client.query(text, (err, res) => {
+    if (err) {
+      console.log("query error", err.message);
+    } else {
+      console.log("result", res.rows[0]);
+    }
+  });
+
+});
+*/
+
+const pg = require('pg');
+
+const configs = {
+    user: 'nausheen',
+    host: '127.0.0.1',
+    database: 'todo',
+    port: 5432,
+};
+
+const client = new pg.Client(configs);
+client.connect((err) => {
+    if( err ){
+        console.log( "error", err.message );
+    }
+    let action = process.argv[2];
+    let actionName = process.argv[3];
+    let values;
+    if (action == "show"){
+        if (err){
+            console.log("error", err.message);
+        }
+        const text = 'SELECT * FROM items';
+        client.query(text, (err, res) => {
+            if (err) {
+                console.log("query error", err.message);
+            }else{
+                for (var i = 0; i < res.rows.length; i++) {
+                    console.log(res.rows[i].id+". [ ]  "+res.rows[i].name)
+                }
+                     // console.log("result", res.rows);
+                client.end();
+            }
+        });
+    }
+    if (action == "add"){
+        let queryText = 'INSERT INTO items (name) VALUES ($1) RETURNING id';
+        values = [actionName];
+        console.log(values);
+        client.query(queryText,values, (err, res) => {
+            if (err) {
+                console.log("query error", err.message);
+            }else{
+                console.log("created!")
+                 client.end();
+             }
+        })
+    }
+    else {
+        return;
+     }
+
+});
