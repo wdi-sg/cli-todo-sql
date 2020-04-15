76486f2cfc96295810928305e78d7a55b9ea0e23
chelsejw <chelsejw@gmail.com>
2020-04-15 22:41:32 +0800

    add functionality for stats add-time

--- index.js
+++ index.js
@@ -24,51 +24,6 @@ let queryDoneCallback = (err, res) => {
     client.end();
 };
 
-const getAvgTime = (err, res) => {
-    if (err) {
-        console.log("query error", err.message);
-    } else {
-        const array = res.rows
-        const theNums = array.map( item => item.mins_diff);
-        const theSum = theNums.reduce( (a,b) => a + b );
-        const averageMins = (theSum / theNums.length).toFixed(1);
-        colorLog('lavender', `Average minutes to complete a task: ${averageMins}`)
-    }
-    client.end();
-};
-
-const bestWorst = (err, res) => {
-
-    err && console.log(`Query error`, err.message)
-
-    const array = res.rows;
-    const best = array.shift();
-    const worst = array.pop();
-    let itemDisplay = item => `${item.id}. ${item.name} | Completed in ${item.mins_diff} mins`
-    let output = `Fastest item: \n ${itemDisplay(best)} \n\n Slowest item: \n ${itemDisplay(worst)}`
-    colorLog('lavender', output);
-
-    client.end();
-
-}
-
-
-const formatDisplay = res => {
-    res.rows.forEach(item => {
-        if (item.archived) {
-            return;
-        }
-        let box = `[ ]`
-        if (item.completion) {
-            box = `[X]`
-            let output = `${item.id}. ${box} - ${item.name} \n Created: ${item.created_at} \n Completed: ${item.completed_at} \n =======================`;
-            colorLog('green', output);
-        } else {
-            let output = `${item.id}. ${box} - ${item.name} \n Created: ${item.created_at} \n =======================`;
-            colorLog('red', output)
-        }
-    });
-};
 
 
 let clientConnectionCallback = (err) => {
@@ -83,41 +38,45 @@ let clientConnectionCallback = (err) => {
         let text = `INSERT INTO items (name) VALUES ('${inputs}') RETURNING id, name`;
         client.query(text, queryDoneCallback);
 
-     //COMPLETE ITEMS INTO TO-DO LIST.
+        //COMPLETE ITEMS INTO TO-DO LIST.
     } else if (command === 'done') {
         const targetItem = process.argv[3];
         const script = `UPDATE items SET completion = TRUE, completed_at = CURRENT_TIMESTAMP WHERE id=${targetItem}`
         client.query(script, queryDoneCallback);
 
-    //DISPLAY ITEMS INTO TO-DO LIST.
+        //DISPLAY ITEMS INTO TO-DO LIST.
     } else if (command === 'show') {
         client.query(`SELECT * FROM items`, queryDoneCallback);
-    
-    // ARCHIVE AN ITEM IN THE TO-DO LIST.
+
+        // ARCHIVE AN ITEM IN THE TO-DO LIST.
     } else if (command === 'archive') {
         const targetItem = process.argv[3];
         const script = `UPDATE items SET archived = TRUE WHERE id=${targetItem}`
         client.query(script, queryDoneCallback);
-    
-    // UN-ARCHIVE AN ITEM IN THE TO-DO LIST.
+
+        // UN-ARCHIVE AN ITEM IN THE TO-DO LIST.
     } else if (command === 'unarchive') {
         const targetItem = process.argv[3];
         const script = `UPDATE items SET archived = FALSE WHERE id=${targetItem}`
         client.query(script, queryDoneCallback);
-    } else if (command==='stats') {
+    } else if (command === 'stats') {
 
         const statQuery = process.argv[3]
         let script;
 
 
         switch (statQuery) {
-            case 'complete-time': 
-                script = `select *,EXTRACT(EPOCH FROM (completed_at - created_at)/60)  as mins_diff from items`
+            case 'complete-time':
+                script = `SELECT *,EXTRACT(EPOCH FROM (completed_at - created_at)/60)  as mins_diff from items`
                 client.query(script, getAvgTime);
                 break;
             case 'best-worst':
-                script = `select *,EXTRACT(EPOCH FROM (completed_at - created_at)/60) as mins_diff from items ORDER BY mins_diff ASC`
+                script = `SELECT *,EXTRACT(EPOCH FROM (completed_at - created_at)/60) as mins_diff from items ORDER BY mins_diff ASC`
                 client.query(script, bestWorst)
+                break;
+            case 'add-time':
+                script = `SELECT name, date(created_at) AS MYDATE FROM items;`
+                client.query(script, avgItemsPerDay)
                 break;
             default:
                 console.log(`Error.`)
@@ -163,7 +122,7 @@ const colorLog = (col, output) => {
             g = 255;
             b = 255;
             break;
-        default: 
+        default:
             r = 255;
             g = 255;
             b = 255;
@@ -171,4 +130,71 @@ const colorLog = (col, output) => {
     }
 
     console.log(`\x1b[38;2;${r};${g};${b}m%s\x1b[0m`, output);
+}
+
+
+const formatDisplay = res => {
+    res.rows.forEach(item => {
+        if (item.archived) {
+            return;
+        }
+        let box = `[ ]`
+        if (item.completion) {
+            box = `[X]`
+            let output = `${item.id}. ${box} - ${item.name} \n Created: ${item.created_at} \n Completed: ${item.completed_at} \n =======================`;
+            colorLog('green', output);
+        } else {
+            let output = `${item.id}. ${box} - ${item.name} \n Created: ${item.created_at} \n =======================`;
+            colorLog('red', output)
+        }
+    });
+};
+
+
+const getAvgTime = (err, res) => {
+    if (err) {
+        console.log("query error", err.message);
+    } else {
+        const array = res.rows
+        const theNums = array.map(item => item.mins_diff);
+        const theSum = theNums.reduce((a, b) => a + b);
+        const averageMins = (theSum / theNums.length).toFixed(1);
+        colorLog('lavender', `Average minutes to complete a task: ${averageMins}`)
+    }
+    client.end();
+};
+
+const bestWorst = (err, res) => {
+
+    err && console.log(`Query error`, err.message)
+
+    const array = res.rows;
+    const best = array.shift();
+    const worst = array.pop();
+    let itemDisplay = item => `${item.id}. ${item.name} | Completed in ${item.mins_diff} mins`
+    let output = `Fastest item: \n ${itemDisplay(best)} \n\n Slowest item: \n ${itemDisplay(worst)}`
+    colorLog('lavender', output);
+    client.end();
+}
+
+const avgItemsPerDay = (err, res) => {
+    err && console.log(`Query error`, err.message)
+    const array = res.rows;
+    let noOfDays = 0;
+
+    array.forEach((item, index) => {
+        //If this is not the last item in the array, 
+        if (array[index + 1]) {
+            //If the next item does not have a similar date, add to the number of days.
+            if (item.date !== array[index + 1].date) {
+                noOfDays++;
+            }
+            //If this is the last item in the array, save the latest count into the array.
+        } else {
+            noOfDays++;
+        }
+    })
+    const avgNoPerDay = array.length / noOfDays;
+    colorLog('lavender', `Average number of items created per day = ${avgNoPerDay}`);
+    client.end();
 }
