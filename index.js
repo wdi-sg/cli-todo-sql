f3c3cd73a2a90e9681c81fbe1b9d10f7cfed9248
chelsejw <chelsejw@gmail.com>
2020-04-15 19:50:46 +0800

    add ability to see created_at, updated_at, as well as archive and unarchive items

--- index.js
+++ index.js
@@ -27,10 +27,21 @@ let queryDoneCallback = (err, result) => {
 
 const showResults = res => {
     res.rows.forEach(item => {
+
+        if (item.archived) {
+            return;
+        }
+
         let box = `[ ]`
-        item.completion ? box = `[X]` : box
-        console.log(`${item.id}. ${box} - ${item.name}`);
-    });
+        if (item.completion) {
+            box = `[X]`
+            let output = `${item.id}. ${box} - ${item.name} \n Created: ${item.created_at} \n Completed: ${item.completed_at} \n =======================`;
+            colorLog('green', output);
+        } else {
+            let output = `${item.id}. ${box} - ${item.name} \n Created: ${item.created_at} \n =======================`;
+            colorLog('red', output)
+        }
+     });
 };
 
 
@@ -50,7 +61,7 @@ let clientConnectionCallback = (err) => {
     } else if (command === 'done') {
 
         const targetItem = process.argv[3];
-        const script = `UPDATE items SET completion = TRUE WHERE id=${targetItem}`
+        const script = `UPDATE items SET completion = TRUE, completed_at = CURRENT_TIMESTAMP WHERE id=${targetItem}`
         client.query(script, queryDoneCallback);
 
     } else if (command === 'show') {
@@ -58,6 +69,15 @@ let clientConnectionCallback = (err) => {
         const script = `SELECT * FROM items`
         displayList();
 
+    } else if (command === 'archive') {
+        const targetItem = process.argv[3];
+        const script = `UPDATE items SET archived = TRUE WHERE id=${targetItem}`
+        client.query(script, queryDoneCallback);
+
+    } else if (command==='unarchive') {
+        const targetItem = process.argv[3];
+        const script = `UPDATE items SET archived = FALSE WHERE id=${targetItem}`
+        client.query(script, queryDoneCallback);
     }
 
 };
@@ -68,3 +88,27 @@ const displayList = ()=> {
 }
 
 client.connect(clientConnectionCallback);
+
+
+
+//Function to format colors of console logs: lavender, green or red.
+const colorLog = (col, output) => {
+    let r;
+    let g;
+    let b
+    if (col === 'lavender') {
+        r = 181;
+        g = 126;
+        b = 220
+    } else if (col === 'red') {
+        r = 230;
+        g = 0;
+        b = 0;
+    } else if (col === 'green') {
+        r = 0;
+        g = 230;
+        b = 0
+    }
+
+    console.log(`\x1b[38;2;${r};${g};${b}m%s\x1b[0m`, output);
+}
