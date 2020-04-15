46f5ac4f12ee7d07feac41d1624e181a2c9f79d5
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-04-15 23:10:14 +0800

    Able to archive data by inserting archived data into archive table and deleting archived data from items table

--- index.js
+++ index.js
@@ -11,30 +11,9 @@ const configs = {
 
 const client = new pg.Client(configs);
 
+// For showing items in the todolist
 let queryDoneCallback = (err, result) => {
-    if (err) {
-      console.log("query error", err.message);
-    }
-    else {
-      console.log(result.rows);
-      console.log(
-        `
- ____  _____  ____    __    ____  ___  ____
-(  _ \\(  _  )(  _ \\  (  )  (_  _)/ __)(_  _)
- ) _ < )(_)(  ) _ <   )(__  _)(_ \\__ \\  )(
-(____/(_____)(____/  (____)(____)(___/ (__)
 
-        `
-        );
-        result.rows.forEach(el => {
-            console.log(`
-${el.id}. ${el.name} - [${el.done}]
-created_at: ${el.created_date}
-updated_at: ${el.updated_date}
-            `);
-        })
-    }
-    client.end();
 };
 
 let clientConnectionCallback = (err) => {
@@ -42,11 +21,37 @@ let clientConnectionCallback = (err) => {
   if( err ){
     console.log( "Connection callback error", err.message );
   }
+  // Show items in todo list
   else if (process.argv[2] === 'show'){
     let text = "select * from items";
 
-    client.query(text, queryDoneCallback);
+    client.query(text, (err, result) => {
+        if (err) {
+          console.log("query error", err.message);
+        }
+        else {
+          console.log(result.rows);
+          console.log(
+            `
+     ____  _____  ____    __    ____  ___  ____
+    (  _ \\(  _  )(  _ \\  (  )  (_  _)/ __)(_  _)
+     ) _ < )(_)(  ) _ <   )(__  _)(_ \\__ \\  )(
+    (____/(_____)(____/  (____)(____)(___/ (__)
+
+            `
+            );
+            result.rows.forEach(el => {
+                console.log(`
+    ${el.id}. ${el.name} - [${el.done}]
+    created_at: ${el.created_date}
+    updated_at: ${el.updated_date}
+                `);
+            })
+        }
+        client.end();
+    });
   }
+  // Add items in todo list
   else if (process.argv[2] === 'add'){
     let text = "insert into items (name, done) values ($1, $2) returning id";
 
@@ -63,7 +68,7 @@ let clientConnectionCallback = (err) => {
         client.end();
     });
   }
-
+  // Mark item as done
   else if(process.argv[2] === 'done'){
     let text = `update items set done='X' where id=${process.argv[3]}`;
 
@@ -78,6 +83,34 @@ let clientConnectionCallback = (err) => {
         client.end();
     })
   }
+  // Archive item by copying row into another table and deleting from current table
+  else if (process.argv[2] === 'archive'){
+    let text1 = `insert into archive (select * from items where id=${process.argv[3]})`;
+
+    let text2 = `delete from items where id=${process.argv[3]}`;
+
+    // Insert archived data into separate archive table
+    client.query(text1, (err, result) => {
+        if (err) {
+            console.log("query error: " + err)
+        }
+        else{
+            console.log("insert into archive table Successful!")
+        }
+    })
+
+    // Delete archive data from items table
+    client.query(text2, (err, result) => {
+        if (err) {
+            console.log("query error: " + err)
+        }
+        else{
+            console.log("delete from items table Successful!")
+        }
+        client.end();
+    })
+  }
+
 
 };
 
