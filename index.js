11d9cb6042f3a8695a883439836faad339c86bdc
ReadyHash <Readyhash@gmail.com>
2020-04-16 08:59:48 +0800

    Added more commands and a global status to check them

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-console.log("/////////////////////////////////////");
-console.log("///////// TO DO LIST PROGRAM ////////");
-console.log("/////////////////////////////////////");
+
+
+
+
+
+
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+console.log("~~~~~~~~~ TO DO LIST PROGRAM ~~~~~~~~");
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
 
 const pg = require('pg');
@@ -12,36 +18,46 @@ const configs = {
     port: 5432,
 };
 
-const addCommand = process.argv[2];
-
-console.log("Checking for command")
+const consoleCommand = process.argv[2];
 
 // A function that reads what command user inputted
 checkCommand = (input) => {
-    if( addCommand == "add"){
-        console.log("-----------------------");
-        console.log("INITIALIZE : ADD")
+    console.log("Checking for command")
+    const client = new pg.Client(configs);
+    let commandStatus = false
 
-        const client = new pg.Client(configs);
+    if( consoleCommand == "add"){
+        commandStatus = true;
+        console.log("-----------------------");
+        console.log("INITIALIZE COMMAND: ADD")
 
         let queryDoneCallback = (err, result) => {
             if (err) {
-              console.log("////////////////////////");
-              console.log("query error", err.message);
-              console.log("////////////////////////");
+                console.log("////////////////////////");
+                console.log("ADD ERROR");
+                console.log(err.message);
+                console.log("////////////////////////");
             } else {
-              console.log("result", result.rows );
+                // ???
+                // how do I view what is inside result.rows object?
+                console.log("result", result.rows );
             }
+            // ???
+            // why put this code outside of else block?
             console.log("-----------------------");
-            console.log("ending connection...");
+            console.log("DISCONNECTING DATABASE")
             client.end();
+            console.log("connection ended");
         };
 
-        // only activates when connection is successful
+        // Only activates when connection is successful
         let clientConnectionCallback = (err) => {
           // checks if connection has errors
           if( err ){
-            console.log( "error", err.message );
+            console.log("////////////////////////");
+            console.log("CONNECTION ERROR :");
+            console.log( err.message );
+            console.log("////////////////////////");
           }
           // new data directory
           let text = "INSERT INTO todoitems (status, instructions) VALUES ($1, $2) RETURNING id";
@@ -55,12 +71,34 @@ checkCommand = (input) => {
         console.log("-----------------------");
         console.log("making the connection to database [todo]...");
         client.connect(clientConnectionCallback);
-
-    }else{
+    }
+    // command to edit the list
+    if( consoleCommand == "edit"){
+        commandStatus = true;
         console.log("-----------------------");
-        console.log("ERROR: COMMAND NOT FOUND");
-        console.log("entered command: " + addCommand);
-        }
+        console.log(" edit command found")
+        console.log(client);
+    }
+    // command to delete one list
+    if( consoleCommand == "delete"){
+        commandStatus = true;
+        console.log("-----------------------");
+        console.log(" delete command found")
+    }
+    // command to view the list
+    if( consoleCommand == "view"){
+        commandStatus = true;
+        console.log("-----------------------");
+        console.log(" view command found")
+
+    }
+    // checks if command does not exist
+    if(commandStatus === false){
+    console.log("////////////////////////");
+    console.log("ERROR: COMMAND DOES NOT EXIST");
+    console.log("entered command: " + consoleCommand);
+    console.log("////////////////////////");
+    }
 };
 
 checkCommand();
