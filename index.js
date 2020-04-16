c7049fbc34279d894b3d2957541a56136105b391
marctanyh <marctanyh@gmail.com>
2020-04-16 09:23:42 +0800

    Marcus - cli-todo-sql
    Part 1

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-console.log("works!!", process.argv[2]);
+ console.log("works!!", process.argv[2]);
 
 const pg = require('pg');
 
+const todo = process.argv[3]
+
 const configs = {
-    user: 'akira',
-    host: '127.0.0.1',
-    database: 'todo',
+    user: "marcustan",
+    host: "127.0.0.1",
+    database: "todo",
     port: 5432,
 };
 
 const client = new pg.Client(configs);
 
+///////////////////////////////////////
+
+let input= process.argv;
+
 let queryDoneCallback = (err, result) => {
     if (err) {
       console.log("query error", err.message);
     } else {
+      
       console.log("result", result.rows );
     }
     client.end();
@@ -24,13 +31,26 @@ let clientConnectionCallback = (err) => {
 
   if( err ){
     console.log( "error", err.message );
+  } if (process.argv[2] === "add") {
+    let text = "INSERT INTO items (name) VALUES ($1) RETURNING id";
+    const values = [todo]; 
+    client.query(text, values, queryDoneCallback);
   }
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+  if (process.argv[2] === "show") {
+    let text = "SELECT * FROM items";
+    client.query(text, (err, res) => {
+      for (i = 0; i < res.rows.length; i++)
+      console.log(res.rows[i]);
+  });
+}
+  
 
-  const values = ["hello"];
+  // let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
 
-  client.query(text, values, queryDoneCallback);
+  // const values = ["hello"];
+
+  // client.query(text, values, queryDoneCallback);
 };
 
 client.connect(clientConnectionCallback);
