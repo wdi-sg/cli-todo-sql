4c874d23cd68d59f09fb4469e37cfff9645c3efa
dannst <arcanic@gmail.com>
2020-04-16 10:27:41 +0800

    add and show functionalities, done without ORDER

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-console.log("works!!", process.argv[2]);
 
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
+    user: 'Mac',
     host: '127.0.0.1',
     database: 'todo',
     port: 5432,
@@ -11,26 +10,51 @@ const configs = {
 
 const client = new pg.Client(configs);
 
-let queryDoneCallback = (err, result) => {
+///////////////////////////////////////////////
+
+const user_command = process.argv[2];
+const input_index = process.argv[3];
+
+
+const renderList = function(res){
+  const list = res.rows;
+  list.forEach(obj => console.log(`${obj.id}. ${obj.done} - ${obj.thing}`));
+}
+
+//callback function after sending query
+const queryDoneCallback = (err, res) => {
     if (err) {
       console.log("query error", err.message);
     } else {
-      console.log("result", result.rows );
+      // console.log(res.rows);
+      renderList(res);
     }
     client.end();
 };
 
-let clientConnectionCallback = (err) => {
+
+//callback function after successful connection
+const clientConnectionCallback = (err) => {
+  let queryText = undefined;
+  let values = undefined;
 
   if( err ){
     console.log( "error", err.message );
   }
 
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
+  if (user_command === 'add'){
+    queryText = 'INSERT INTO todolist (thing, done) VALUES ($1, $2) RETURNING';
+    values = [process.argv[3], '[ ]']; 
 
-  const values = ["hello"];
+  } else if (user_command === 'show'){
+    queryText = 'SELECT * FROM todolist;';
+  } else if (user_command === 'done'){
+    queryText = `UPDATE todolist SET done = '[X]' WHERE id = ${input_index}`;
+  }
 
-  client.query(text, values, queryDoneCallback);
+  //send query after connection
+  client.query(queryText, values, queryDoneCallback);
 };
 
+//connects to pg with a callback function
 client.connect(clientConnectionCallback);
