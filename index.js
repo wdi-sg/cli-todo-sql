09e2c24d664df073ae4e17d1fa782a3d03f6d893
ReadyHash <Readyhash@gmail.com>
2020-04-16 09:25:48 +0800

    Added a way to view list table in console

--- index.js
+++ index.js
@@ -23,14 +23,18 @@ const consoleCommand = process.argv[2];
 // A function that reads what command user inputted
 checkCommand = (input) => {
     console.log("Checking for command")
+
     const client = new pg.Client(configs);
+
+    // create a global status to make sure only correct commands are true
     let commandStatus = false
 
     if( consoleCommand == "add"){
         commandStatus = true;
         console.log("-----------------------");
-        console.log("INITIALIZE COMMAND: ADD")
+        console.log("INITIALIZE COMMAND: ADD");
 
+        // This function runs if query returns a response
         let queryDoneCallback = (err, result) => {
             if (err) {
                 console.log("////////////////////////");
@@ -47,11 +51,11 @@ checkCommand = (input) => {
             console.log("-----------------------");
             console.log("DISCONNECTING DATABASE")
             client.end();
-            console.log("connection ended");
         };
 
+
         // Only activates when connection is successful
-        let clientConnectionCallback = (err) => {
+        let addnewList = (err) => {
           // checks if connection has errors
           if( err ){
             console.log("////////////////////////");
@@ -59,39 +63,92 @@ checkCommand = (input) => {
             console.log( err.message );
             console.log("////////////////////////");
           }
-          // new data directory
+          // specifies where to add the data
           let text = "INSERT INTO todoitems (status, instructions) VALUES ($1, $2) RETURNING id";
           // retrieve input from process to fill data
-          const values = [process.argv[3], process.argv[4]];
+          const inputData = [process.argv[3], process.argv[4]];
 
-          client.query(text, values, queryDoneCallback);
+          client.query(text, inputData, queryDoneCallback);
           console.log("-----------------------");
           console.log("finished query...");
         };
         console.log("-----------------------");
         console.log("making the connection to database [todo]...");
-        client.connect(clientConnectionCallback);
+        client.connect(addnewList);
     }
+
+
+
+
+
     // command to edit the list
     if( consoleCommand == "edit"){
         commandStatus = true;
         console.log("-----------------------");
-        console.log(" edit command found")
-        console.log(client);
+        console.log("INITIALIZE COMMAND: EDIT");
+        console.log();
     }
+
+
+
+
+
     // command to delete one list
     if( consoleCommand == "delete"){
         commandStatus = true;
         console.log("-----------------------");
-        console.log(" delete command found")
+        console.log("INITIALIZE COMMAND: DELETE");
     }
+
+
+
+
+
     // command to view the list
     if( consoleCommand == "view"){
         commandStatus = true;
         console.log("-----------------------");
-        console.log(" view command found")
+        console.log("INITIALIZE COMMAND: VIEW");
 
+        let queryDoneCallback = (err, result) => {
+            if (err) {
+                console.log("////////////////////////");
+                console.log("ADD ERROR");
+                console.log(err.message);
+                console.log("////////////////////////");
+            } else {
+                // ???
+                // how do I view what is inside result.rows object?
+                console.table( result.rows );
+            }
+            // ???
+            // why put this code outside of else block?
+            console.log("-----------------------");
+            console.log("DISCONNECTING DATABASE")
+            client.end();
+        };
+
+        viewList = (err) => {
+            if(err){
+                console.log("////////////////////////");
+                console.log("CONNECTION ERROR :");
+                console.log( err.message );
+                console.log("////////////////////////");
+            }
+            let listTable = "SELECT * FROM todoitems";
+
+            client.query(listTable, queryDoneCallback);
+
+        }
+        console.log("-----------------------");
+        console.log("making the connection to database [todo]...");
+        client.connect(viewList)
     }
+
+
+
+
+
     // checks if command does not exist
     if(commandStatus === false){
     console.log("////////////////////////");
