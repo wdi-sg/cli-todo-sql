ec3534b75096e690756208ce371c75cd474bae7b
“loribean” <aureliadotlim@gmail.com>
2020-08-26 18:24:59 +0800

    initial commit: able to add into todo database

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 console.log("works!!", process.argv[2]);
-
+const operation = process.argv[2];
+const chore = process.argv[3];
+let choreStatus = false;
 const pg = require('pg');
 
 const configs = {
-    user: 'akira',
+    user: 'aurelialim',
     host: '127.0.0.1',
     database: 'todo',
     port: 5432,
@@ -11,26 +13,22 @@ const configs = {
 
 const client = new pg.Client(configs);
 
-let queryDoneCallback = (err, result) => {
-    if (err) {
-      console.log("query error", err.message);
+
+
+client.connect((error)=> {
+    if(error){
+        console.log('ERROR AT CONNECT', error.message)
     } else {
-      console.log("result", result.rows );
+        if(operation ==="add"){
+            queryText = 'INSERT INTO items (name, done) VALUES ($1, $2) RETURNING id';
+            values = [chore, choreStatus];
+            client.query(queryText,values, (err, res) => {
+            if (err) {
+            console.log("query error", err.message);
+            } else {
+            console.log("result", res.rows);
     }
-    client.end();
-};
-
-let clientConnectionCallback = (err) => {
-
-  if( err ){
-    console.log( "error", err.message );
-  }
-
-  let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
-
-  const values = ["hello"];
-
-  client.query(text, values, queryDoneCallback);
-};
-
-client.connect(clientConnectionCallback);
+  });
+        }
+    }
+})
