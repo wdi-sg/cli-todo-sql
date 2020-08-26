fe1a0d746a9f7b75b60517d0980115273e6a2e4d
exhlim <exhlimm@gmail.com>
2020-08-27 02:38:44 +0800

    sei-24-Eugene

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("works!!", process.argv[2]);
 var formPhrase = require("font-ascii").default;
 const pg = require('pg');
 const configs = {
@@ -10,7 +9,7 @@ const configs = {
 const pool = new pg.Pool(configs);
 
 let argv = process.argv.splice(2);
-let archiveIndex = [2, 4, 5];
+let archiveIndex = [];
 let i = 0;
 let checkbox = "[ ] - ";
 let donebox = "[X] - ";
@@ -33,61 +32,42 @@ switch(argv[0].toLowerCase()) {
                 } else {
                     let day = response.rows[i].log.getDate(), month = response.rows[i].log.getMonth(), year = response.rows[i].log.getFullYear();
                     let hours = response.rows[i].log.getHours(), minutes = response.rows[i].log.getMinutes(), seconds = response.rows[i].log.getSeconds();
-                    console.log(`${response.rows[i].id}. ${response.rows[i].checkbox}${response.rows[i].item}.------>Created on ${day}/${month}/${year} ${hours}:${minutes}:${seconds}`)
+                    if(response.rows[i].done == null){
+                        console.log(`${response.rows[i].id}. ${response.rows[i].checkbox}${response.rows[i].item}.------>Created on ${day}/${month}/${year} ${hours}:${minutes}:${seconds}`)
+                    } else {
+                        let comp = ((response.rows[i].done - response.rows[i].log)/(1000 * 3600))%24;
+                        console.log(`${response.rows[i].id}. ${response.rows[i].checkbox}${response.rows[i].item}.------>Created on ${day}/${month}/${year} ${hours}:${minutes}:${seconds} Average completion time: ${comp.toFixed(2)} hours`)
+                    }
                 }
             }
-            // response.rows.forEach(row => {
-            //     // if(archiveIndex[i] == row.id) {
-            //     //     i++;
-            //     // } else {
-            //         let day = row.log.getDate(), month = row.log.getMonth(), year = row.log.getFullYear();
-            //         let hours = row.log.getHours(), minutes = row.log.getMinutes(), seconds = row.log.getSeconds();
-            //         console.log(`${row.id}. ${row.checkbox}${row.item}.------>Created on ${day}/${month}/${year} ${hours}:${minutes}:${seconds}`)
-            //     // }
-            // })
         })
     } break;
     case "done": {
         let index = parseInt(argv[1]);
-        let queryUpdate = `UPDATE todo SET checkbox='${donebox}' WHERE id='${index}'`
+        let queryUpdate = `UPDATE todo SET (checkbox,done) = ('${donebox}','${Date.now()}') WHERE id='${index}'`
         formPhrase("Done", { typeface: "StarWars", color: "green" });
-        pool.query(queryUpdate, (err, response) => {})
+        pool.query(queryUpdate, (err, response) => {
+            if(err) console.log(err);
+        })
         } break;
-    // case "archive": {
-    //     archiveIndex = .push(parseInt(argv[1]));
-
-    // }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let queryDoneCallback = (err, result) => {
-    if (err) {
-      console.log("query error", err.message);
-    } else {
-      console.log("result", result.rows );
+    case "archive": {
+        formPhrase("Archived", { typeface: "StarWars", color: "white" });
+        console.log("Type 'node index.js show' to see the results")
+        archiveIndex.push((parseInt(argv[1])));
+    } break;
+    case "stats": {
+        let query = `SELECT * FROM todo ORDER BY done`
+        pool.query(query, (err, response) => {
+            let rows = response.rows;
+            console.log(`Task with the longest time: ${response.rows[0].item}`)
+            for(let i = response.rows.length - 1; i > 0; i--) {
+                if(response.rows[i].done == null){
+                    continue;
+                } else {
+                    console.log(`Task with the longest time: ${response.rows[i].item}`)
+                    break;
+                }
+            }
+        })
     }
-    pool.end();
-};
-
-// let poolConnectionCallback = (err) => {
-//   if( err ){
-//     console.log( "error", err.message );
-//   }
-//   let text = "INSERT INTO todo (name) VALUES ($1) RETURNING id";
-//   const values = ["hello"];
-//   pool.query(text, values, queryDoneCallback);
-// };
-// pool.connect(poolConnectionCallback);
+}
